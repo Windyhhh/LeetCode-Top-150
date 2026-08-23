ee43498bbf2825aeb7fcf1fe7cdf87ca6d09872a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:01 +0800

    Add ?????/105._???????????????/code_templates/solution.scala

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def buildTree(preorder: Array[Int], inorder: Array[Int]): TreeNode = {
+        
+    }
+}
