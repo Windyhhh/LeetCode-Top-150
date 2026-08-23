c51c9886013cb50cbf1a22d62ed03b6d220ce785
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:48 +0800

    Add ???/125._?????/code_templates/solution.kt

---
+++ 双指针/125._验证回文串/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 验证回文串
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-palindrome/
+
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+        
+    }
+}
