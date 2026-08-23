5a26cc0f2b31be999bb6878ac789fda348151f6b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:46 +0800

    Add ??/56._????/code_templates/solution.kt

---
+++ 区间/56._合并区间/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 合并区间
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+class Solution {
+    fun merge(intervals: Array<IntArray>): Array<IntArray> {
+        
+    }
+}
