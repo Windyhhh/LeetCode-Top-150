7007dc1add272081e68e43e8db10ecf544104442
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:20 +0800

    Add ???/128._??????/code_templates/solution.c

---
+++ 哈希表/128._最长连续序列/code_templates/solution.c
@@ -0,0 +1,8 @@
+# 最长连续序列
+# Language: c
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+int longestConsecutive(int* nums, int numsSize) {
+    
+}
