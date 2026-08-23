108086535594293133d4b89c668fb61fef8c0204
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:21 +0800

    Add ???/383._???/code_templates/solution.rb

---
+++ 哈希表/383._赎金信/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 赎金信
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+# @param {String} ransom_note
+# @param {String} magazine
+# @return {Boolean}
+def can_construct(ransom_note, magazine)
+    
+end
