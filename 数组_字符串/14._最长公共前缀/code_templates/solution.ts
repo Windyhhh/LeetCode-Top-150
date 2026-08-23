cdfdc706cb61836421b12339b7a32096000c86c5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:05 +0800

    Add ??_???/14._??????/code_templates/solution.ts

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 最长公共前缀
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+function longestCommonPrefix(strs: string[]): string {
+    
+};
