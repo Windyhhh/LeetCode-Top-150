d086b73f90ae8f0066839b479d3bf32c28eec2ee
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:52 +0800

    Add ??/56._????/code_templates/solution.swift

---
+++ 区间/56._合并区间/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 合并区间
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+class Solution {
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        
+    }
+}
