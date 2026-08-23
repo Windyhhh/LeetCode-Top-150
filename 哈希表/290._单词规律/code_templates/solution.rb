83fd80102503af2c51a15fd6c24b7238a6fe901c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:02 +0800

    Add ???/290._????/code_templates/solution.rb

---
+++ 哈希表/290._单词规律/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 单词规律
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+# @param {String} pattern
+# @param {String} s
+# @return {Boolean}
+def word_pattern(pattern, s)
+    
+end
