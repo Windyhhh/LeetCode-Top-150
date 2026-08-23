b8a9679f4353a57569f6751920f61fb59620d5a0
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:18 +0800

    Add ?????/106._???????????????/code_templates/solution.rb

---
+++ 二叉树通用/106._从中序与后序遍历序列构造二叉树/code_templates/solution.rb
@@ -0,0 +1,20 @@
+# 从中序与后序遍历序列构造二叉树
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {Integer[]} inorder
+# @param {Integer[]} postorder
+# @return {TreeNode}
+def build_tree(inorder, postorder)
+    
+end
