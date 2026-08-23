c68c023865f1dca5563667ad5f7a15905b2c3a19
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:31 +0800

    Add ???/128._??????/code_templates/solution.scala

---
+++ 哈希表/128._最长连续序列/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 最长连续序列
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+object Solution {
+    def longestConsecutive(nums: Array[Int]): Int = {
+        
+    }
+}
