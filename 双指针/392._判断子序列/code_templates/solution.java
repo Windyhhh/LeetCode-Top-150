65a47ffe505c5fd0300f9a8fd08fe2e225b5806f
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:44 +0800

    Add ???/392._?????/code_templates/solution.java

---
+++ 双指针/392._判断子序列/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 判断子序列
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+    }
+}
