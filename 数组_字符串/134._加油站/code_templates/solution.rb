c757188c86b683d197f5eff925b41e37058aaf56
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:19 +0800

    Add ??_???/134._???/code_templates/solution.rb

---
+++ 数组_字符串/134._加油站/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 加油站
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/gas-station/
+
+# @param {Integer[]} gas
+# @param {Integer[]} cost
+# @return {Integer}
+def can_complete_circuit(gas, cost)
+    
+end
