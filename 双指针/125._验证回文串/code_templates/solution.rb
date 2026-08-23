54765a894bd3bdc68cd645827921d06384d3e5d8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:51 +0800

    Add ???/125._?????/code_templates/solution.rb

---
+++ 双指针/125._验证回文串/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 验证回文串
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-palindrome/
+
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+    
+end
