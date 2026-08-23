1ab57eca8feac964ecd7fc7c7bdaf71df148b39a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:34 +0800

    Add ?????/112._????/code_templates/solution.php

---
+++ 二叉树通用/112._路径总和/code_templates/solution.php
@@ -0,0 +1,29 @@
+# 路径总和
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $targetSum
+     * @return Boolean
+     */
+    function hasPathSum($root, $targetSum) {
+        
+    }
+}
