735ff622323a4a8275a7cf9f9d94854e4d0d9891
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:45 +0800

    Add ???/202._???/code_templates/solution.php

---
+++ 哈希表/202._快乐数/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 快乐数
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/happy-number/
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isHappy($n) {
+        
+    }
+}
