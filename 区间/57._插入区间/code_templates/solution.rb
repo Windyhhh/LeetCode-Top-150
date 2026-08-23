0ab589b45a26b8a7dee9ecab5d2825253143be2c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:09 +0800

    Add ??/57._????/code_templates/solution.rb

---
+++ 区间/57._插入区间/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 插入区间
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+# @param {Integer[][]} intervals
+# @param {Integer[]} new_interval
+# @return {Integer[][]}
+def insert(intervals, new_interval)
+    
+end
