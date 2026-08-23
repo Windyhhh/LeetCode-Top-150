9b19ca21fcb9513f4403ae6728d1f03dcf5f4b55
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:17 +0800

    Add ?????/117._????????????????_II/code_templates/solution.scala

---
+++ 二叉树通用/117._填充每个节点的下一个右侧节点指针_II/code_templates/solution.scala
@@ -0,0 +1,20 @@
+# 填充每个节点的下一个右侧节点指针 II
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+/**
+ * Definition for a Node.
+ * class Node(var _value: Int) {
+ *   var value: Int = _value
+ *   var left: Node = null
+ *   var right: Node = null
+ *   var next: Node = null
+ * }
+ */
+
+object Solution {
+    def connect(root: Node): Node = {
+        
+    }
+}
