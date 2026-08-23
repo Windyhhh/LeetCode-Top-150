040572e4a8e7b9583d36082db4313b7f6aa467d2
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:27 +0800

    Add ???/219._??????_II/code_templates/solution.scala

---
+++ 哈希表/219._存在重复元素_II/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 存在重复元素 II
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/contains-duplicate-ii/
+
+object Solution {
+    def containsNearbyDuplicate(nums: Array[Int], k: Int): Boolean = {
+        
+    }
+}
