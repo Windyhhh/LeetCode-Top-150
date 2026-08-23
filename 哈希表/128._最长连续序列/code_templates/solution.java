9bfdc1f76bfd1b8d427988dd4870a37cbb6aa435
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:24 +0800

    Add ???/128._??????/code_templates/solution.java

---
+++ 哈希表/128._最长连续序列/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 最长连续序列
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+    }
+}
