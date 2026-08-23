f51495cc052ec2c9e1bb657e6718dc196f9168ae
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:02:53 +0800

    Add ?????/100._????/code_templates/solution.kt

---
+++ 二叉树通用/100._相同的树/code_templates/solution.kt
@@ -0,0 +1,20 @@
+# 相同的树
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/same-tree/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        
+    }
+}
