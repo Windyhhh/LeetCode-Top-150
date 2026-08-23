14d85222292ce3a98360de98f52a47dad1a83bb0
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:50 +0800

    Add ?????/114._????????/code_templates/solution.cs

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.cs
@@ -0,0 +1,23 @@
+# 二叉树展开为链表
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public void Flatten(TreeNode root) {
+        
+    }
+}
