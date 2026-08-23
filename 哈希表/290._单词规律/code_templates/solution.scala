e76c5abb98895a8b0216a2d3c894fca8a45000ef
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:04 +0800

    Add ???/290._????/code_templates/solution.scala

---
+++ 哈希表/290._单词规律/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 单词规律
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+object Solution {
+    def wordPattern(pattern: String, s: String): Boolean = {
+        
+    }
+}
