8a8e562703408933d4c3b4ae513c2aab828eb2d3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:46 +0800

    Add ???/242._????????/code_templates/solution.scala

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 有效的字母异位词
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+object Solution {
+    def isAnagram(s: String, t: String): Boolean = {
+        
+    }
+}
