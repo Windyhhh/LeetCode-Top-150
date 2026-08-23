6e48750154e822845c7fffea73226ac6ba226dae
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:18 +0800

    Add ??_???/121._?????????/code_templates/solution.py

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 买卖股票的最佳时机
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
