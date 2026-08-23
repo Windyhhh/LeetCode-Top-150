5420a760e5235f21b899674cf464873dc7bacd3d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:04 +0800

    Add ???/1._????/code_templates/solution.java

---
+++ 哈希表/1._两数之和/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 两数之和
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+    }
+}
