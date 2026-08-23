7cc56eed5a27054ef44d9cb3f6dd0ca826097e7c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:20 +0800

    Add ??_???/121._?????????/code_templates/solution.rs

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 买卖股票的最佳时机
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        
+    }
+}
