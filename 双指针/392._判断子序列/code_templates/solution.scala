3f027afd4bb6bef28bee130b4e75334ea60d2890
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:52 +0800

    Add ???/392._?????/code_templates/solution.scala

---
+++ 双指针/392._判断子序列/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 判断子序列
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+object Solution {
+    def isSubsequence(s: String, t: String): Boolean = {
+        
+    }
+}
