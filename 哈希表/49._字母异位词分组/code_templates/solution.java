009603ea885e0a6dfce8b5bc74ab5dad3ebe6aab
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:35 +0800

    Add ???/49._???????/code_templates/solution.java

---
+++ 哈希表/49._字母异位词分组/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 字母异位词分组
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/group-anagrams/
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+    }
+}
