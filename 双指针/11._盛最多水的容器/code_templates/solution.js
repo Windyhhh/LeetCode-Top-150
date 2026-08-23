2bad92ac83c29eeb819fcb1becf745fbcb306171
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:27 +0800

    Add ???/11._???????/code_templates/solution.js

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.js
@@ -0,0 +1,12 @@
+# 盛最多水的容器
+# Language: javascript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+};
