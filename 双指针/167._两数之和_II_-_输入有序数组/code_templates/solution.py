083aff7ac7d860509144e7fe63f1efe2e3afc038
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:28 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.py

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 两数之和 II - 输入有序数组
+# Language: python3
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
