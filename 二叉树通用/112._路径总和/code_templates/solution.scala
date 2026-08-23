d530a106b5bc3bf13c083a4c0b2808b1233c8caa
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:39 +0800

    Add ?????/112._????/code_templates/solution.scala

---
+++ 二叉树通用/112._路径总和/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 路径总和
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def hasPathSum(root: TreeNode, targetSum: Int): Boolean = {
+        
+    }
+}
