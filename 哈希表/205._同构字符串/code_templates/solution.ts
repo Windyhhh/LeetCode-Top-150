68e9219885bb865a1787a0314e8c28a7513a5067
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:10 +0800

    Add ???/205._?????/code_templates/solution.ts

---
+++ 哈希表/205._同构字符串/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 同构字符串
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+function isIsomorphic(s: string, t: string): boolean {
+    
+};
