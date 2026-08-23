e83f827980db52b3414f7ae3236508a0e6bc5bdf
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:11 +0800

    Add ???/1._????/code_templates/solution.rs

---
+++ 哈希表/1._两数之和/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 两数之和
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        
+    }
+}
