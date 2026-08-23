04543bfa022630e79cb261fea37aa67ac82a6db7
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:56 +0800

    Add ?????/114._????????/code_templates/solution.rb

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.rb
@@ -0,0 +1,19 @@
+# 二叉树展开为链表
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Void} Do not return anything, modify root in-place instead.
+def flatten(root)
+    
+end
