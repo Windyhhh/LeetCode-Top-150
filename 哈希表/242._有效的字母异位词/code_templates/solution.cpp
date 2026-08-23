81c455c2fb5d8b04bcc12149359154373fd67ff1
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:37 +0800

    Add ???/242._????????/code_templates/solution.cpp

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 有效的字母异位词
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+    }
+};
