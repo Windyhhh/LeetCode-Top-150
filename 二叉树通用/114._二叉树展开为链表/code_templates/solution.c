446d31a177d14fdd669500e5c43d813ed1732b72
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:48 +0800

    Add ?????/114._????????/code_templates/solution.c

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.c
@@ -0,0 +1,16 @@
+# 二叉树展开为链表
+# Language: c
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+void flatten(struct TreeNode* root) {
+    
+}
