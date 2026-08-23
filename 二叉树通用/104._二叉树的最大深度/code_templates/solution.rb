c7f0af8359715b5bf15d9810676e9ea504da1501
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:40 +0800

    Add ?????/104._????????/code_templates/solution.rb

---
+++ 二叉树通用/104._二叉树的最大深度/code_templates/solution.rb
@@ -0,0 +1,19 @@
+# 二叉树的最大深度
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+    
+end
