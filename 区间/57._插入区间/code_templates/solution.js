6b77a38a7df1f4292b92d2d66dc196fa10030b7c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:05 +0800

    Add ??/57._????/code_templates/solution.js

---
+++ 区间/57._插入区间/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 插入区间
+# Language: javascript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    
+};
