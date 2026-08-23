aeceff377c8ccd5cfa3a8f62cf4c2609b62c381a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:33 +0800

    Add ???/128._??????/code_templates/solution.ts

---
+++ 哈希表/128._最长连续序列/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 最长连续序列
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+function longestConsecutive(nums: number[]): number {
+    
+};
