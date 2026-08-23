60816dde31ed4967659af2c5a44b0ca1b7fb197a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:42 +0800

    Add ??_???/135._????/code_templates/solution.scala

---
+++ 数组_字符串/135._分发糖果/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 分发糖果
+# Language: scala
+# Difficulty: Hard
+# Problem Link: https://leetcode.cn/problems/candy/
+
+object Solution {
+    def candy(ratings: Array[Int]): Int = {
+        
+    }
+}
