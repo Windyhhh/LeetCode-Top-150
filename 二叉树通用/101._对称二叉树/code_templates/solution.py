5f61ed4f55b3172eba3f454d3561248a0698d6d4
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:03:19 +0800

    Add ?????/101._?????/code_templates/solution.py

---
+++ 二叉树通用/101._对称二叉树/code_templates/solution.py
@@ -0,0 +1,14 @@
+# 对称二叉树
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/symmetric-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
