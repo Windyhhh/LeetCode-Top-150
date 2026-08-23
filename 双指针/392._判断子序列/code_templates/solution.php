9f5a57616cbfdaed2bcad020526b20088483502e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:48 +0800

    Add ???/392._?????/code_templates/solution.php

---
+++ 双指针/392._判断子序列/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 判断子序列
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @param String $t
+     * @return Boolean
+     */
+    function isSubsequence($s, $t) {
+        
+    }
+}
