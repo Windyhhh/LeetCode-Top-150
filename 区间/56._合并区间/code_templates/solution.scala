778f8882efeb11bf866e7bc4d29d391d6db52449
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:51 +0800

    Add ??/56._????/code_templates/solution.scala

---
+++ 区间/56._合并区间/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 合并区间
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+object Solution {
+    def merge(intervals: Array[Array[Int]]): Array[Array[Int]] = {
+        
+    }
+}
