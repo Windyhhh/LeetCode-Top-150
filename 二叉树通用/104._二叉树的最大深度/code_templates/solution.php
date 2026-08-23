f96bbdc56901e05fbab71a290acba46b29d1e4e7
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:38 +0800

    Add ?????/104._????????/code_templates/solution.php

---
+++ 二叉树通用/104._二叉树的最大深度/code_templates/solution.php
@@ -0,0 +1,28 @@
+# 二叉树的最大深度
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function maxDepth($root) {
+        
+    }
+}
