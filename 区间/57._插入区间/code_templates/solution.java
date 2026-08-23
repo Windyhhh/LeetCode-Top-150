ab808e06ed4fb221e78158fead8e87d4536435ac
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:04 +0800

    Add ??/57._????/code_templates/solution.java

---
+++ 区间/57._插入区间/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 插入区间
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+    }
+}
