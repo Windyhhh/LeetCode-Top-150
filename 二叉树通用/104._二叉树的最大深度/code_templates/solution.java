6aea8bce3170cf2c0d33273c13e64002d940e045
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:35 +0800

    Add ?????/104._????????/code_templates/solution.java

---
+++ 二叉树通用/104._二叉树的最大深度/code_templates/solution.java
@@ -0,0 +1,25 @@
+# 二叉树的最大深度
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+    }
+}
