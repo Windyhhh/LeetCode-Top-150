644c143e8a300247ff308123e68c744980936e90
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:05 +0800

    Add ??/228._????/code_templates/solution.java

---
+++ 区间/228._汇总区间/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 汇总区间
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        
+    }
+}
