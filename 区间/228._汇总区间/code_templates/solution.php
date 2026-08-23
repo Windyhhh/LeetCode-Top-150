b0f771ea34e10c9ef02fb5ec347b4c1eaa1b2dbc
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:09 +0800

    Add ??/228._????/code_templates/solution.php

---
+++ 区间/228._汇总区间/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 汇总区间
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return String[]
+     */
+    function summaryRanges($nums) {
+        
+    }
+}
