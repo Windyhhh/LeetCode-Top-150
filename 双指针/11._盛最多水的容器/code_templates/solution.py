d0dfb1d642b854ef07b14474f2ce0e0e2e500eda
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:30 +0800

    Add ???/11._???????/code_templates/solution.py

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 盛最多水的容器
+# Language: python3
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
