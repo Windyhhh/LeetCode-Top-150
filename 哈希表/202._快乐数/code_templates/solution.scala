9e3e3fabb952bb5dec52663b8a71173758a01b40
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:50 +0800

    Add ???/202._???/code_templates/solution.scala

---
+++ 哈希表/202._快乐数/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 快乐数
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/happy-number/
+
+object Solution {
+    def isHappy(n: Int): Boolean = {
+        
+    }
+}
