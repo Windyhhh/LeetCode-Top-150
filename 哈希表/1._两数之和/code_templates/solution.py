bb0402e230558e6a2b9620bc2c1f3c225b11858c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:08 +0800

    Add ???/1._????/code_templates/solution.py

---
+++ 哈希表/1._两数之和/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 两数之和
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
