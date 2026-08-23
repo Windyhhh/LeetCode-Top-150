62119e9b95b8d000d2c99f777536125d36d9d805
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:04 +0800

    Add ??_???/14._??????/code_templates/solution.swift

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 最长公共前缀
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        
+    }
+}
