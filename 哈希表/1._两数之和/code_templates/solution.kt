2f85f5b25e51988cf9993662e8b25c93f4aaaba8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:06 +0800

    Add ???/1._????/code_templates/solution.kt

---
+++ 哈希表/1._两数之和/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 两数之和
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        
+    }
+}
