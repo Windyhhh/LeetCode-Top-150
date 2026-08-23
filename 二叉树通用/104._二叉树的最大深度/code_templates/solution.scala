dc8bb6d2e18276b7fff6d832cd35d6e69cba6718
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:42 +0800

    Add ?????/104._????????/code_templates/solution.scala

---
+++ 二叉树通用/104._二叉树的最大深度/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 二叉树的最大深度
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def maxDepth(root: TreeNode): Int = {
+        
+    }
+}
