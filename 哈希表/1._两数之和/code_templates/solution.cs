9a2ae26826a13e1006f341b5bd7c9b8e25faba1a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:03 +0800

    Add ???/1._????/code_templates/solution.cs

---
+++ 哈希表/1._两数之和/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 两数之和
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+    }
+}
