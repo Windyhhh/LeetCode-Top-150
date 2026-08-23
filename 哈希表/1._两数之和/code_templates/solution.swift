e61f7bc8c49486ee1ac976c88c548834fd9c2e8e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:13 +0800

    Add ???/1._????/code_templates/solution.swift

---
+++ 哈希表/1._两数之和/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 两数之和
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+    }
+}
