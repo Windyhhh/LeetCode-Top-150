350f19d4781f64d1e49ff33be5d943f9c194fde8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:13 +0800

    Add ??/228._????/code_templates/solution.scala

---
+++ 区间/228._汇总区间/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 汇总区间
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+object Solution {
+    def summaryRanges(nums: Array[Int]): List[String] = {
+        
+    }
+}
