eddb14839b212e5d82ed5fb0261eabf5629726da
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:33 +0800

    Add ?????/112._????/code_templates/solution.kt

---
+++ 二叉树通用/112._路径总和/code_templates/solution.kt
@@ -0,0 +1,20 @@
+# 路径总和
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
+        
+    }
+}
