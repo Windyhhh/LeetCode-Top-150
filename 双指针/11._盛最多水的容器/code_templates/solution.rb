a0f93c6112f42d63cf947ce06059398d02984553
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:31 +0800

    Add ???/11._???????/code_templates/solution.rb

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 盛最多水的容器
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+# @param {Integer[]} height
+# @return {Integer}
+def max_area(height)
+    
+end
