2bc8f7d1812f09efc7b868a09b9037d7877b5076
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:22 +0800

    Add ???/383._???/code_templates/solution.rs

---
+++ 哈希表/383._赎金信/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 赎金信
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+impl Solution {
+    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+        
+    }
+}
