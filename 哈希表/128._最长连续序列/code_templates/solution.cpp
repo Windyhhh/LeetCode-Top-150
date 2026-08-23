62ecefb061a2d0515829901928d26fbfddccbc45
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:21 +0800

    Add ???/128._??????/code_templates/solution.cpp

---
+++ 哈希表/128._最长连续序列/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 最长连续序列
+# Language: cpp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+    }
+};
