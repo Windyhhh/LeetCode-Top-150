eb410e482a88274e6381d9940594282d55d88cd5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:29 +0800

    Add ???/11._???????/code_templates/solution.php

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 盛最多水的容器
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height) {
+        
+    }
+}
