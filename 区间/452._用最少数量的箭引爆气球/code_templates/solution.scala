06d4e2bc704902034a6e8f4a7105a097ffac2d65
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:32 +0800

    Add ??/452._???????????/code_templates/solution.scala

---
+++ 区间/452._用最少数量的箭引爆气球/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 用最少数量的箭引爆气球
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons/
+
+object Solution {
+    def findMinArrowShots(points: Array[Array[Int]]): Int = {
+        
+    }
+}
