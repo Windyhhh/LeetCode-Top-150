7cf4e3e34530a6d41ca7b25c7e4d82cbcc7cf4cf
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:13 +0800

    Add ???/383._???/code_templates/solution.cpp

---
+++ 哈希表/383._赎金信/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 赎金信
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+    }
+};
