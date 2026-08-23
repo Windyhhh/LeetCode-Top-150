085fb69ce9c844f190dee53c72e70eee18c13e87
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:52 +0800

    Add ?????/114._????????/code_templates/solution.js

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.js
@@ -0,0 +1,20 @@
+# 二叉树展开为链表
+# Language: javascript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    
+};
