7371278d25dfdba0251f5d3140faf0ac72cc66bb
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:01 +0800

    Add ???/290._????/code_templates/solution.py

---
+++ 哈希表/290._单词规律/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 单词规律
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        
