2e109f0f57a84072721b6f9a88854ed9fc5a9baa
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:28 +0800

    Add ???/128._??????/code_templates/solution.py

---
+++ 哈希表/128._最长连续序列/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 最长连续序列
+# Language: python3
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
