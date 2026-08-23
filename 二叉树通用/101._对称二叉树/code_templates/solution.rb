899163fefba6d3048a38a2d9a33962508ca26f37
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:03:21 +0800

    Add ?????/101._?????/code_templates/solution.rb

---
+++ 二叉树通用/101._对称二叉树/code_templates/solution.rb
@@ -0,0 +1,19 @@
+# 对称二叉树
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/symmetric-tree/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Boolean}
+def is_symmetric(root)
+    
+end
