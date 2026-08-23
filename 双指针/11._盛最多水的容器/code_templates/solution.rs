65f407c4e3cb02ab995f8303c1ba1feed2581bd9
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:32 +0800

    Add ???/11._???????/code_templates/solution.rs

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 盛最多水的容器
+# Language: rust
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        
+    }
+}
