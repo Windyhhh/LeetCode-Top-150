e727f07dd6849c2cb663ff03109493c84bc7ab8d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:35 +0800

    Add ???/11._???????/code_templates/solution.ts

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 盛最多水的容器
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+function maxArea(height: number[]): number {
+    
+};
