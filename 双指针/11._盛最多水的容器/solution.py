f6e1a16f0ca19b9ce5e9c5483d69553de1b4c81e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:18 +0800

    Add ???/11._???????/solution.py

---
+++ 双指针/11._盛最多水的容器/solution.py
@@ -0,0 +1,21 @@
+# 盛最多水的容器
+# 题目编号: 11
+# 难度: Medium
+# 题目链接: https://leetcode.cn/problems/container-with-most-water/
+# 标签: 贪心, 数组, 双指针
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+
+# 解题思路:
+# 1. 
+# 2. 
+# 3. 
+
+# 时间复杂度: O()
+# 空间复杂度: O()
