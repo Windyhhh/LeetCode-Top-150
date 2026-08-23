eaddbb4c9ba4cba1cf9291696d1e90ee61798ef4
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:28 +0800

    Add ?????/129._????????????/code_templates/solution.java

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.java
@@ -0,0 +1,25 @@
+# 求根节点到叶节点数字之和
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        
+    }
+}
