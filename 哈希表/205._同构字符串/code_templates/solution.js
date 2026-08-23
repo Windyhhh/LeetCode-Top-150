6d1c924de5645157ed39d4fa831690ad42e8b1f6
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:02 +0800

    Add ???/205._?????/code_templates/solution.js

---
+++ 哈希表/205._同构字符串/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 同构字符串
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    
+};
