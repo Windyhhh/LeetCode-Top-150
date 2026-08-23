6cef655b00f9a94d41e12600ec3c91ffc5a0976a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:20 +0800

    Add ???/219._??????_II/code_templates/solution.java

---
+++ 哈希表/219._存在重复元素_II/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 存在重复元素 II
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/contains-duplicate-ii/
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+    }
+}
