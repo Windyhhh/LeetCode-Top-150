fb70bc55941311fe4c3bbcd7e11d25a01573a96d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:17 +0800

    Add ??_???/134._???/code_templates/solution.php

---
+++ 数组_字符串/134._加油站/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 加油站
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/gas-station/
+
+class Solution {
+
+    /**
+     * @param Integer[] $gas
+     * @param Integer[] $cost
+     * @return Integer
+     */
+    function canCompleteCircuit($gas, $cost) {
+        
+    }
+}
