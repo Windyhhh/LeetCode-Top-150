3b52f9d42fd6567f0a67e7d8757ea19dbcab950e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:09 +0800

    Add ???/205._?????/code_templates/solution.swift

---
+++ 哈希表/205._同构字符串/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 同构字符串
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+class Solution {
+    func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        
+    }
+}
