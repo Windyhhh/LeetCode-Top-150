7c4cf0dffe8172c85aa93ae55bf437007c793fc5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:54 +0800

    Add ???/392._?????/code_templates/solution.ts

---
+++ 双指针/392._判断子序列/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 判断子序列
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+function isSubsequence(s: string, t: string): boolean {
+    
+};
