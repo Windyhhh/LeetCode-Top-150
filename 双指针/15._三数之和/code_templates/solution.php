ec2b5490508489af3c2c59109db02de18349561d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:08 +0800

    Add ???/15._????/code_templates/solution.php

---
+++ 双指针/15._三数之和/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 三数之和
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/3sum/
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    function threeSum($nums) {
+        
+    }
+}
