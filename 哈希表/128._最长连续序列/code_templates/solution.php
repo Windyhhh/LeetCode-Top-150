c410f3c5bb4a9781843375a22f201ebc8109cc00
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:27 +0800

    Add ???/128._??????/code_templates/solution.php

---
+++ 哈希表/128._最长连续序列/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 最长连续序列
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function longestConsecutive($nums) {
+        
+    }
+}
