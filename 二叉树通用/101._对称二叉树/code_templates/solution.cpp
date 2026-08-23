fb858f7caa8c0c383501c0aba7bfd1da7b6b58f4
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:03:12 +0800

    Add ?????/101._?????/code_templates/solution.cpp

---
+++ 二叉树通用/101._对称二叉树/code_templates/solution.cpp
@@ -0,0 +1,22 @@
+# 对称二叉树
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/symmetric-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        
+    }
+};
