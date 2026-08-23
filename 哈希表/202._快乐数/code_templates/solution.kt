9f6ff587b3fd5c6e3d218e44d5956c04205b72ff
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:44 +0800

    Add ???/202._???/code_templates/solution.kt

---
+++ 哈希表/202._快乐数/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 快乐数
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/happy-number/
+
+class Solution {
+    fun isHappy(n: Int): Boolean {
+        
+    }
+}
