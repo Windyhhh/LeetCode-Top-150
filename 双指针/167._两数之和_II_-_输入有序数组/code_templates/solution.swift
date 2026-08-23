f09a3b0d1cf615bc27682a668632984f2d740f56
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:33 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.swift

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 两数之和 II - 输入有序数组
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        
+    }
+}
