97355f5f99323ff1fbcd1776df74bc98f35ea495
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:07 +0800

    Add ???/1._????/code_templates/solution.php

---
+++ 哈希表/1._两数之和/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 两数之和
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        
+    }
+}
