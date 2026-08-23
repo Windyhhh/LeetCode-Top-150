66ad7e6f9a0c0d3d2cd867d903fcc2a1dad9c3a0
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:13 +0800

    Add ??/57._????/code_templates/solution.swift

---
+++ 区间/57._插入区间/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 插入区间
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+class Solution {
+    func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
+        
+    }
+}
