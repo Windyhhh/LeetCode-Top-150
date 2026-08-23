cf4b956bcd046eb8b46c3b1e1739fd4f5667aa6a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:28 +0800

    Add ???/11._???????/code_templates/solution.kt

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 盛最多水的容器
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+class Solution {
+    fun maxArea(height: IntArray): Int {
+        
+    }
+}
