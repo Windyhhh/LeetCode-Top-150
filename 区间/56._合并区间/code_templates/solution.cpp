031ecee6925fc191e627d68e2cb8e976f282febc
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:41 +0800

    Add ??/56._????/code_templates/solution.cpp

---
+++ 区间/56._合并区间/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 合并区间
+# Language: cpp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+    }
+};
