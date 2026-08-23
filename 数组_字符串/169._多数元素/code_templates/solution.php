71d7bef50a08013dab78adbe83136ce62ae237b0
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:37 +0800

    Add ??_???/169._????/code_templates/solution.php

---
+++ 数组_字符串/169._多数元素/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 多数元素
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/majority-element/
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function majorityElement($nums) {
+        
+    }
+}
