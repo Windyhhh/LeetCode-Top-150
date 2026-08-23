9bc40b5fd0d11e3d9a8da09c705705f7a07cea51
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:25 +0800

    Add ???/128._??????/code_templates/solution.kt

---
+++ 哈希表/128._最长连续序列/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 最长连续序列
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        
+    }
+}
