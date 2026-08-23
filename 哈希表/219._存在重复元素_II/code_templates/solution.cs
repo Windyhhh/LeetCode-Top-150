f66c76b1c9b5246586d4597c7021ff38d2a258fa
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:18 +0800

    Add ???/219._??????_II/code_templates/solution.cs

---
+++ 哈希表/219._存在重复元素_II/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 存在重复元素 II
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/contains-duplicate-ii/
+
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        
+    }
+}
