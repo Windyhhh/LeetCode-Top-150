b565317747d8dc3e91793cd1e0000edd1d8c2722
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:53 +0800

    Add ???/125._?????/code_templates/solution.scala

---
+++ 双指针/125._验证回文串/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 验证回文串
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-palindrome/
+
+object Solution {
+    def isPalindrome(s: String): Boolean = {
+        
+    }
+}
