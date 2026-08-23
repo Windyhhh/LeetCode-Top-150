e8bede67d0c76bb28cd8caea481602745d0d4375
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:39 +0800

    Add ???/242._????????/code_templates/solution.java

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 有效的字母异位词
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+    }
+}
