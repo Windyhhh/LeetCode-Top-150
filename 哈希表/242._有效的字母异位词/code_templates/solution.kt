692582bbc52d5e00f33c2cbcd4f4bbc8abc3c230
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:41 +0800

    Add ???/242._????????/code_templates/solution.kt

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 有效的字母异位词
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        
+    }
+}
