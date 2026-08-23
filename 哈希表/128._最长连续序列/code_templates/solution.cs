d62737fa088b9fd735241a60f5dc5e5d7f47c667
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:22 +0800

    Add ???/128._??????/code_templates/solution.cs

---
+++ 哈希表/128._最长连续序列/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 最长连续序列
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        
+    }
+}
