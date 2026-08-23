df4597ca68219c8fb05a3e7fc6c7a3c4c4bc0cf3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:05 +0800

    Add ???/1._????/code_templates/solution.js

---
+++ 哈希表/1._两数之和/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 两数之和
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+};
