b5cf83ceb9f0eebfb1bfc9ba5b7335ff1947661f
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:57 +0800

    Add ???/290._????/code_templates/solution.java

---
+++ 哈希表/290._单词规律/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 单词规律
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        
+    }
+}
