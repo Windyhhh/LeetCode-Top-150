f51e6914b36cc6529183e8eba723362d76067d89
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:48 +0800

    Add ?????/226._?????/code_templates/solution.js

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.js
@@ -0,0 +1,20 @@
+# 翻转二叉树
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    
+};
