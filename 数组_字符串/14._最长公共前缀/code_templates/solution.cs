c257e59bfd328114dd70ff1b9db9d888a6fa0914
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:54 +0800

    Add ??_???/14._??????/code_templates/solution.cs

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 最长公共前缀
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        
+    }
+}
