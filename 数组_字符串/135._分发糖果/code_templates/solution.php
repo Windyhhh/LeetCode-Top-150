cb389dc33150e548ddeade8b1aa1143cc3bed899
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:38 +0800

    Add ??_???/135._????/code_templates/solution.php

---
+++ 数组_字符串/135._分发糖果/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 分发糖果
+# Language: php
+# Difficulty: Hard
+# Problem Link: https://leetcode.cn/problems/candy/
+
+class Solution {
+
+    /**
+     * @param Integer[] $ratings
+     * @return Integer
+     */
+    function candy($ratings) {
+        
+    }
+}
