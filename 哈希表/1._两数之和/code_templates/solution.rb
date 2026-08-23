1d28cc3e5c22acfe1a4e43911cf5efa1963c6736
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:10 +0800

    Add ???/1._????/code_templates/solution.rb

---
+++ 哈希表/1._两数之和/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 两数之和
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    
+end
