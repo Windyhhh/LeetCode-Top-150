245c2895f28f876663e4e5635569e2d6c5531cde
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:42 +0800

    Add ???/49._???????/code_templates/solution.scala

---
+++ 哈希表/49._字母异位词分组/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 字母异位词分组
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/group-anagrams/
+
+object Solution {
+    def groupAnagrams(strs: Array[String]): List[List[String]] = {
+        
+    }
+}
