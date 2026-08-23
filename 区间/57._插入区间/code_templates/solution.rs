d000732cfc3cf7ef7049dfb208539b03b5bc492d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:11 +0800

    Add ??/57._????/code_templates/solution.rs

---
+++ 区间/57._插入区间/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 插入区间
+# Language: rust
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+impl Solution {
+    pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+        
+    }
+}
