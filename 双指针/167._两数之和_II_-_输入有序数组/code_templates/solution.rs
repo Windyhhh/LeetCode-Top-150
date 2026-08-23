1b6b7a803e73b9ae77e3f40e7a6e58d8d16825d6
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:31 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.rs

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 两数之和 II - 输入有序数组
+# Language: rust
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        
+    }
+}
