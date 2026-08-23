90ba0fe237c2c25e384bf3a3154a20b63a02b6c8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:25 +0800

    Add ???/11._???????/code_templates/solution.cs

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 盛最多水的容器
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+public class Solution {
+    public int MaxArea(int[] height) {
+        
+    }
+}
