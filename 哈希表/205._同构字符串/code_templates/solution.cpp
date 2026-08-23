ce294b821de020f45d2db267c6c592bbba5c2293
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:59 +0800

    Add ???/205._?????/code_templates/solution.cpp

---
+++ 哈希表/205._同构字符串/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 同构字符串
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+    }
+};
