7a63148742e97eda0d75aca60b48092bf95cf5cd
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:43 +0800

    Add ???/242._????????/code_templates/solution.py

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 有效的字母异位词
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
