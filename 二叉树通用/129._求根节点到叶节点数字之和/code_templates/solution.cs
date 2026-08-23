7eb08e4435ba7784228a352b085a8046cbc2b7ce
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:27 +0800

    Add ?????/129._????????????/code_templates/solution.cs

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.cs
@@ -0,0 +1,23 @@
+# 求根节点到叶节点数字之和
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int SumNumbers(TreeNode root) {
+        
+    }
+}
