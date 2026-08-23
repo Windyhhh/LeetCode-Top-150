909a6c787da14b16a863472e431cfc8dfa699e16
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:54 +0800

    Add ?????/114._????????/code_templates/solution.php

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.php
@@ -0,0 +1,28 @@
+# 二叉树展开为链表
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return NULL
+     */
+    function flatten($root) {
+        
+    }
+}
