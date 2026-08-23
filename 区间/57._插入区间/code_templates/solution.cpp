924a4a774c9e33dce2ba67ede036f13b6c89007b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:02 +0800

    Add ??/57._????/code_templates/solution.cpp

---
+++ 区间/57._插入区间/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 插入区间
+# Language: cpp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+    }
+};
