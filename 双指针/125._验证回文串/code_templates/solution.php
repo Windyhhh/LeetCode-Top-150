4dc69b62e70f3a5692d1e0371c3ef4586008a77e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:49 +0800

    Add ???/125._?????/code_templates/solution.php

---
+++ 双指针/125._验证回文串/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 验证回文串
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-palindrome/
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isPalindrome($s) {
+        
+    }
+}
