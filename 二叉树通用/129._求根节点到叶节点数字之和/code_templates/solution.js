2bc15881d742bb656267d7b31d710285b6d2baf7
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:29 +0800

    Add ?????/129._????????????/code_templates/solution.js

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.js
@@ -0,0 +1,20 @@
+# 求根节点到叶节点数字之和
+# Language: javascript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    
+};
