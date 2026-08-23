710a56e332fc10bf076127505635067adc6cb39e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:13 +0800

    Add ?????/117._????????????????_II/code_templates/solution.php

---
+++ 二叉树通用/117._填充每个节点的下一个右侧节点指针_II/code_templates/solution.php
@@ -0,0 +1,26 @@
+# 填充每个节点的下一个右侧节点指针 II
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+/**
+ * Definition for a Node.
+ * class Node {
+ *     function __construct($val = 0) {
+ *         $this->val = $val;
+ *         $this->left = null;
+ *         $this->right = null;
+ *         $this->next = null;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param Node $root
+     * @return Node
+     */
+    public function connect($root) {
+        
+    }
+}
