6fd52d6590a762dc39d65b6260977fd25f4719f8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:30 +0800

    Add ???/128._??????/code_templates/solution.rs

---
+++ 哈希表/128._最长连续序列/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 最长连续序列
+# Language: rust
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+impl Solution {
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        
+    }
+}
