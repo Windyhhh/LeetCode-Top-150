16940f4280283045cbbdd6d26266b7ae5b0e81a7
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:58 +0800

    Add ?????/114._????????/code_templates/solution.scala

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 二叉树展开为链表
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def flatten(root: TreeNode): Unit = {
+        
+    }
+}
