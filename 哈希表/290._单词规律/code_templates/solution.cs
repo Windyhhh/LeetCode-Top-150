09dd6ffa26ffcbe8d17722d3a73aa18ca8322e46
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:56 +0800

    Add ???/290._????/code_templates/solution.cs

---
+++ 哈希表/290._单词规律/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 单词规律
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+public class Solution {
+    public bool WordPattern(string pattern, string s) {
+        
+    }
+}
