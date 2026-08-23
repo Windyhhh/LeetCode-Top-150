2bfae24d5db11a08d5b9cb9f438a18a0e6c0446e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:26 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.kt

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 两数之和 II - 输入有序数组
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        
+    }
+}
