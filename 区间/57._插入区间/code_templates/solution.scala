80971e5b5b4126d83863f53ff64671834db55ed9
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:12 +0800

    Add ??/57._????/code_templates/solution.scala

---
+++ 区间/57._插入区间/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 插入区间
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+object Solution {
+    def insert(intervals: Array[Array[Int]], newInterval: Array[Int]): Array[Array[Int]] = {
+        
+    }
+}
