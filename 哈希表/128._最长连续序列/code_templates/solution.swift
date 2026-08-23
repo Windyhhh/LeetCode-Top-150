b3dd322c1d9c2ffbc4ad11d5d136920f7af4d98d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:32 +0800

    Add ???/128._??????/code_templates/solution.swift

---
+++ 哈希表/128._最长连续序列/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 最长连续序列
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        
+    }
+}
