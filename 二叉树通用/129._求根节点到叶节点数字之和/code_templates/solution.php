540d79412262270b4e6e7fd570b810b9aad3a1e5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:31 +0800

    Add ?????/129._????????????/code_templates/solution.php

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.php
@@ -0,0 +1,28 @@
+# 求根节点到叶节点数字之和
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function sumNumbers($root) {
+        
+    }
+}
