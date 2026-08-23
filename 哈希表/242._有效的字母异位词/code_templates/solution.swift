1456ccd0197b42aa6e4b9b97a01dfbbb41e99648
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:47 +0800

    Add ???/242._????????/code_templates/solution.swift

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 有效的字母异位词
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        
+    }
+}
