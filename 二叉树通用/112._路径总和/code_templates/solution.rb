e184a65d773216c174323ef76ceffd796e2c21a2
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:36 +0800

    Add ?????/112._????/code_templates/solution.rb

---
+++ 二叉树通用/112._路径总和/code_templates/solution.rb
@@ -0,0 +1,20 @@
+# 路径总和
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} target_sum
+# @return {Boolean}
+def has_path_sum(root, target_sum)
+    
+end
