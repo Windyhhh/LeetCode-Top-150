10e3dc450b9bce823782b748595b36e7af32a22f
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:03 +0800

    Add ??/228._????/code_templates/solution.cpp

---
+++ 区间/228._汇总区间/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 汇总区间
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        
+    }
+};
