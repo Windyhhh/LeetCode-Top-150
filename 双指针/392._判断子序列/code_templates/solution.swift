233c97f36afc41dbeff9dd09d410331b7e10fa44
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:53 +0800

    Add ???/392._?????/code_templates/solution.swift

---
+++ 双指针/392._判断子序列/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 判断子序列
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+class Solution {
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        
+    }
+}
