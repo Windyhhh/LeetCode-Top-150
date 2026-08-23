cc3b4aacc913b9da2922a13bb87388919ab06bed
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:18 +0800

    Add ?????/117._????????????????_II/code_templates/solution.swift

---
+++ 二叉树通用/117._填充每个节点的下一个右侧节点指针_II/code_templates/solution.swift
@@ -0,0 +1,26 @@
+# 填充每个节点的下一个右侧节点指针 II
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var left: Node?
+ *     public var right: Node?
+ *	   public var next: Node?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func connect(_ root: Node?) -> Node? {
+        
+    }
+}
