22049315a3c65d806e8f6e3206de3627149be382
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:51 +0800

    Add ???/202._???/code_templates/solution.swift

---
+++ 哈希表/202._快乐数/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 快乐数
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/happy-number/
+
+class Solution {
+    func isHappy(_ n: Int) -> Bool {
+        
+    }
+}
