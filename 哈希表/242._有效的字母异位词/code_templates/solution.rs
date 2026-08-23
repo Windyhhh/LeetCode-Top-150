0f67fb38ae4f094a917ffcc2a2e25ebb040a69c3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:45 +0800

    Add ???/242._????????/code_templates/solution.rs

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 有效的字母异位词
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        
+    }
+}
