83e98775cc5656a8cb08b779a11774b7964a3c0e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:30 +0800

    Add ??/452._???????????/code_templates/solution.rb

---
+++ 区间/452._用最少数量的箭引爆气球/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 用最少数量的箭引爆气球
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons/
+
+# @param {Integer[][]} points
+# @return {Integer}
+def find_min_arrow_shots(points)
+    
+end
