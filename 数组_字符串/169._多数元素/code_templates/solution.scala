7c844287a2c6c1d851f9b169942969415e40549d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:41 +0800

    Add ??_???/169._????/code_templates/solution.scala

---
+++ 数组_字符串/169._多数元素/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 多数元素
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/majority-element/
+
+object Solution {
+    def majorityElement(nums: Array[Int]): Int = {
+        
+    }
+}
