900d987a93d97ed33e30741c3bf72f7097859fa9
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:06 +0800

    Add ???/290._????/code_templates/solution.ts

---
+++ 哈希表/290._单词规律/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 单词规律
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+function wordPattern(pattern: string, s: string): boolean {
+    
+};
