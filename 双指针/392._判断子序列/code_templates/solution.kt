7a5a0b32d98962e870c1251f9a037c480bb464a5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:47 +0800

    Add ???/392._?????/code_templates/solution.kt

---
+++ 双指针/392._判断子序列/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 判断子序列
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+class Solution {
+    fun isSubsequence(s: String, t: String): Boolean {
+        
+    }
+}
