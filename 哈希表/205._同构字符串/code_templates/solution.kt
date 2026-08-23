461031144ff14f1a94f57be94eb30671bbed9e9d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:03 +0800

    Add ???/205._?????/code_templates/solution.kt

---
+++ 哈希表/205._同构字符串/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 同构字符串
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+class Solution {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        
+    }
+}
