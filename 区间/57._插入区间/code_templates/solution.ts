7d4509a13370e27b70bc435fd692c09bdade4e12
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:14 +0800

    Add ??/57._????/code_templates/solution.ts

---
+++ 区间/57._插入区间/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 插入区间
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+    
+};
