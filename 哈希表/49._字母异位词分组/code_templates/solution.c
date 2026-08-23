e5b5ebad6ef9b83a615e478a0d18dc420f1a18cb
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:31 +0800

    Add ???/49._???????/code_templates/solution.c

---
+++ 哈希表/49._字母异位词分组/code_templates/solution.c
@@ -0,0 +1,13 @@
+# 字母异位词分组
+# Language: c
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/group-anagrams/
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+char*** groupAnagrams(char** strs, int strsSize, int* returnSize, int** returnColumnSizes) {
+    
+}
