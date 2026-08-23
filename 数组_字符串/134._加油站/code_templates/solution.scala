6fbe4769e8686ae590b98e0b7560b418057ec54f
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:22 +0800

    Add ??_???/134._???/code_templates/solution.scala

---
+++ 数组_字符串/134._加油站/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 加油站
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/gas-station/
+
+object Solution {
+    def canCompleteCircuit(gas: Array[Int], cost: Array[Int]): Int = {
+        
+    }
+}
