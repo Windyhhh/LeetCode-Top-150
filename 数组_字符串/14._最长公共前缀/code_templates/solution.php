b6175827d4e86272d8aceb65d640b7661476f946
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:58 +0800

    Add ??_???/14._??????/code_templates/solution.php

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 最长公共前缀
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+        
+    }
+}
