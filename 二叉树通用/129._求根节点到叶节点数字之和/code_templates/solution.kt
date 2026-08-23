139fac65bfbe19f8fbea416b37be73369428f432
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:30 +0800

    Add ?????/129._????????????/code_templates/solution.kt

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.kt
@@ -0,0 +1,20 @@
+# 求根节点到叶节点数字之和
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun sumNumbers(root: TreeNode?): Int {
+        
+    }
+}
