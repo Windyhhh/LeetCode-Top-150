5cabe46159ed972f0e10648ec5dc646bd9ba095d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:07 +0800

    Add ???/205._?????/code_templates/solution.rs

---
+++ 哈希表/205._同构字符串/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 同构字符串
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+impl Solution {
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        
+    }
+}
