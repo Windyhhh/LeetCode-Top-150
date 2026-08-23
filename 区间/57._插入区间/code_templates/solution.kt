a24b8f25841203f7f8ffdf5bff000cc034d3c797
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:06 +0800

    Add ??/57._????/code_templates/solution.kt

---
+++ 区间/57._插入区间/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 插入区间
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+class Solution {
+    fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+        
+    }
+}
