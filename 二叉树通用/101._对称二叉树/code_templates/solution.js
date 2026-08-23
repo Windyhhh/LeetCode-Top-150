d8a9e187e702c679b5a0f7f5b23143e654412dc4
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:03:16 +0800

    Add ?????/101._?????/code_templates/solution.js

---
+++ 二叉树通用/101._对称二叉树/code_templates/solution.js
@@ -0,0 +1,20 @@
+# 对称二叉树
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/symmetric-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    
+};
