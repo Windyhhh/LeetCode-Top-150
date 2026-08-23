fb76c029a0405efe46685bdd88d9b32c827b0721
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:23 +0800

    Add ???/11._???????/code_templates/solution.cpp

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 盛最多水的容器
+# Language: cpp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+    }
+};
