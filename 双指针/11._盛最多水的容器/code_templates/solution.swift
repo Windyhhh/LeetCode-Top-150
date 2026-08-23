0dd6de75823ed570a98822fbbc7d12ad7d446aa1
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:34 +0800

    Add ???/11._???????/code_templates/solution.swift

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 盛最多水的容器
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        
+    }
+}
