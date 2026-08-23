723fe02f107e5b4ae65f33936bd13bf866f654a3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:00 +0800

    Add ??_???/14._??????/code_templates/solution.py

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 最长公共前缀
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
