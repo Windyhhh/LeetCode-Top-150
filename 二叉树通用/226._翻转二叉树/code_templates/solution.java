5b1d7cf04eaae541fcae28b1c26ace5d9c50bc99
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:47 +0800

    Add ?????/226._?????/code_templates/solution.java

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.java
@@ -0,0 +1,25 @@
+# 翻转二叉树
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+    }
+}
