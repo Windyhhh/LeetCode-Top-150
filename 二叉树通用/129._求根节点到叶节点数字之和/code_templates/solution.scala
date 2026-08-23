65ceaed5611b69d103245d9666a567ed7ff5b8b5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:35 +0800

    Add ?????/129._????????????/code_templates/solution.scala

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 求根节点到叶节点数字之和
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def sumNumbers(root: TreeNode): Int = {
+        
+    }
+}
