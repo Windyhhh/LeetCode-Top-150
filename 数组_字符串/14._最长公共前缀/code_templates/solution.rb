27a726bc4aa24b484dfbf4746774ba126ee220a7
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:01 +0800

    Add ??_???/14._??????/code_templates/solution.rb

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 最长公共前缀
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+    
+end
