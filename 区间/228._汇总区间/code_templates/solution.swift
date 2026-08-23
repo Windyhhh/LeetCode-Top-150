039b7073898600db0c738e400b60db7793beff15
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:14 +0800

    Add ??/228._????/code_templates/solution.swift

---
+++ 区间/228._汇总区间/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 汇总区间
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+class Solution {
+    func summaryRanges(_ nums: [Int]) -> [String] {
+        
+    }
+}
