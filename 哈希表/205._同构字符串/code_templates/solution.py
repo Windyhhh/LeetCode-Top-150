0183bfd9010d4582cfaab99aefe798fb88fae890
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:05 +0800

    Add ???/205._?????/code_templates/solution.py

---
+++ 哈希表/205._同构字符串/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 同构字符串
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
