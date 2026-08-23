687d30981c82e8aae3ad1ac9fc27e182393f6b72
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:49 +0800

    Add ?????/226._?????/code_templates/solution.kt

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.kt
@@ -0,0 +1,20 @@
+# 翻转二叉树
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        
+    }
+}
