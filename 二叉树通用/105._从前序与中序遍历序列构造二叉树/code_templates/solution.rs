75fb57956f90ec51b4b497234172a5ac407a0c88
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:00 +0800

    Add ?????/105._???????????????/code_templates/solution.rs

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.rs
@@ -0,0 +1,30 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: rust
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        
+    }
+}
