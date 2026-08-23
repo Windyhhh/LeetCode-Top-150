886597b08ce1e7ef1a1c4e1453936fab79c33970
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:24 +0800

    Add ???/383._???/code_templates/solution.swift

---
+++ 哈希表/383._赎金信/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 赎金信
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+class Solution {
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        
+    }
+}
