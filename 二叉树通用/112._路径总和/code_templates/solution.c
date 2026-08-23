cb5701decf7a7989a04b9297ebe22b7106b21bab
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:28 +0800

    Add ?????/112._????/code_templates/solution.c

---
+++ 二叉树通用/112._路径总和/code_templates/solution.c
@@ -0,0 +1,16 @@
+# 路径总和
+# Language: c
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+    
+}
