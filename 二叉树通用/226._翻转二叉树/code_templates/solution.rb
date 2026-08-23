5e696264aa77d06737b3bbaaa2ed0da27c962c28
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:52 +0800

    Add ?????/226._?????/code_templates/solution.rb

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.rb
@@ -0,0 +1,19 @@
+# 翻转二叉树
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+    
+end
