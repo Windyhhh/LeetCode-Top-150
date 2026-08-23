d445fd566bf779a0c79ab4dc39fab0c2c110eff6
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:53 +0800

    Add ?????/105._???????????????/code_templates/solution.cs

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.cs
@@ -0,0 +1,23 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode BuildTree(int[] preorder, int[] inorder) {
+        
+    }
+}
