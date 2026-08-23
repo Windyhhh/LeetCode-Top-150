b45a1447882e9f1efe469dc6e01b9d04cd04c2fe
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:57 +0800

    Add ?????/114._????????/code_templates/solution.rs

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.rs
@@ -0,0 +1,30 @@
+# 二叉树展开为链表
+# Language: rust
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
+        
+    }
+}
