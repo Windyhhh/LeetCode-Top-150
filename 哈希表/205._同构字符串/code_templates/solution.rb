5c93f92dd70ae16f9a3f34dbd028c908bb94f774
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:06 +0800

    Add ???/205._?????/code_templates/solution.rb

---
+++ 哈希表/205._同构字符串/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 同构字符串
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_isomorphic(s, t)
+    
+end
