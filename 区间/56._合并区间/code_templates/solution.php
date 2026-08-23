d81faebb99a994e59096c2792472ffcafe98f1b5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:47 +0800

    Add ??/56._????/code_templates/solution.php

---
+++ 区间/56._合并区间/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 合并区间
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+class Solution {
+
+    /**
+     * @param Integer[][] $intervals
+     * @return Integer[][]
+     */
+    function merge($intervals) {
+        
+    }
+}
