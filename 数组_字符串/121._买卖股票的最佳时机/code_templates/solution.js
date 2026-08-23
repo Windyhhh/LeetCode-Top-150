f64c63a384f16f516e67d39bfb261ce0f625971b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:15 +0800

    Add ??_???/121._?????????/code_templates/solution.js

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.js
@@ -0,0 +1,12 @@
+# 买卖股票的最佳时机
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    
+};
