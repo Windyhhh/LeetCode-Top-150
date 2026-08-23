5038d416e0d807e90ca024eafaba18b87d7eed12
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:37 +0800

    Add ?????/129._????????????/code_templates/solution.ts

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.ts
@@ -0,0 +1,22 @@
+# 求根节点到叶节点数字之和
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sumNumbers(root: TreeNode | null): number {
+    
+};
