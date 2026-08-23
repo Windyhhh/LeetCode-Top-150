0bc9605c3359a040cf4e26cbe29c14f426ef10b3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:18 +0800

    Add ???/383._???/code_templates/solution.kt

---
+++ 哈希表/383._赎金信/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 赎金信
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+class Solution {
+    fun canConstruct(ransomNote: String, magazine: String): Boolean {
+        
+    }
+}
