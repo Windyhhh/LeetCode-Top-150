d38e65c2937836f3cc1325a12d87a9d14d36d19d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:32 +0800

    Add ?????/112._????/code_templates/solution.js

---
+++ 二叉树通用/112._路径总和/code_templates/solution.js
@@ -0,0 +1,21 @@
+# 路径总和
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    
+};
