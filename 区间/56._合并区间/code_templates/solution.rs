30c8678d6dfdf49de8c09b77954577de686801a3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:50 +0800

    Add ??/56._????/code_templates/solution.rs

---
+++ 区间/56._合并区间/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 合并区间
+# Language: rust
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+impl Solution {
+    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        
+    }
+}
