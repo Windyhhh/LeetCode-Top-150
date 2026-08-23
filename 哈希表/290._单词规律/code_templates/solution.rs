cf260d7091169436965c2293d76d413c667e6e11
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:03 +0800

    Add ???/290._????/code_templates/solution.rs

---
+++ 哈希表/290._单词规律/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 单词规律
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+impl Solution {
+    pub fn word_pattern(pattern: String, s: String) -> bool {
+        
+    }
+}
