b34c98870b961c379cfa84ba5502743fc8a30d77
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:25 +0800

    Add ???/383._???/code_templates/solution.ts

---
+++ 哈希表/383._赎金信/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 赎金信
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    
+};
