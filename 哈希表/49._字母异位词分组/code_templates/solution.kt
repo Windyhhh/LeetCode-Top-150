88e6edd88dfe22bf7dd71d557200bebf5080c502
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:37 +0800

    Add ???/49._???????/code_templates/solution.kt

---
+++ 哈希表/49._字母异位词分组/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 字母异位词分组
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/group-anagrams/
+
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        
+    }
+}
