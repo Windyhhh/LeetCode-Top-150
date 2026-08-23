4adaf85b633b8a4a2da56712dbbd7664602fb7b0
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:26 +0800

    Add ???/11._???????/code_templates/solution.java

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 盛最多水的容器
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+    }
+}
