c6050ddd6087b483911fe0635481c219568e6075
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:00 +0800

    Add ???/205._?????/code_templates/solution.cs

---
+++ 哈希表/205._同构字符串/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 同构字符串
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        
+    }
+}
