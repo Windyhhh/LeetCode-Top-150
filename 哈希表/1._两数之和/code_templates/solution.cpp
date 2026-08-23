353657010d294fa094896d3d18626db9053f0f37
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:02 +0800

    Add ???/1._????/code_templates/solution.cpp

---
+++ 哈希表/1._两数之和/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 两数之和
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
