cd82eeffb1057b839e3c98067b8878a34e4fa7a3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:08 +0800

    Add ??/57._????/code_templates/solution.py

---
+++ 区间/57._插入区间/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 插入区间
+# Language: python3
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
