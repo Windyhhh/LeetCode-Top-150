c56f98f9c59e622b060f9d60bc6328640d3f6d86
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:21 +0800

    Add ??_???/121._?????????/code_templates/solution.scala

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 买卖股票的最佳时机
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+object Solution {
+    def maxProfit(prices: Array[Int]): Int = {
+        
+    }
+}
