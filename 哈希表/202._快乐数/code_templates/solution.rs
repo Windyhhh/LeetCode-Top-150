9f0c6117222cd62003af6f206c95a8dec7055102
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:48 +0800

    Add ???/202._???/code_templates/solution.rs

---
+++ 哈希表/202._快乐数/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 快乐数
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/happy-number/
+
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {
+        
+    }
+}
