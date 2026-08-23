c9f828d6595d4c8b09b145e29b77e1ea0c9b7ce3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:17 +0800

    Add ??_???/121._?????????/code_templates/solution.php

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 买卖股票的最佳时机
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+
+    /**
+     * @param Integer[] $prices
+     * @return Integer
+     */
+    function maxProfit($prices) {
+        
+    }
+}
