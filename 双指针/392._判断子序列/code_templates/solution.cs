d6bf24f878232548e329e4d69b0f4ed0ebfe4576
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:43 +0800

    Add ???/392._?????/code_templates/solution.cs

---
+++ 双指针/392._判断子序列/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 判断子序列
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        
+    }
+}
