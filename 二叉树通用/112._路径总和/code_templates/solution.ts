82c81f440e0cfc1e769861ce5872bd7337f34576
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:41 +0800

    Add ?????/112._????/code_templates/solution.ts

---
+++ 二叉树通用/112._路径总和/code_templates/solution.ts
@@ -0,0 +1,22 @@
+# 路径总和
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    
+};
