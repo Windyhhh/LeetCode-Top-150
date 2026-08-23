d43b2eebdc3e5cb8f4e6917655e89e2f340a3463
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:25 +0800

    Add ?????/129._????????????/code_templates/solution.c

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.c
@@ -0,0 +1,16 @@
+# 求根节点到叶节点数字之和
+# Language: c
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int sumNumbers(struct TreeNode* root) {
+    
+}
