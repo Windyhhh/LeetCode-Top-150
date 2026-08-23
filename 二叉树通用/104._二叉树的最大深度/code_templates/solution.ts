d2b9938a6dd48761584014f25a3614599ba3faad
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:44 +0800

    Add ?????/104._????????/code_templates/solution.ts

---
+++ 二叉树通用/104._二叉树的最大深度/code_templates/solution.ts
@@ -0,0 +1,22 @@
+# 二叉树的最大深度
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    
+};
