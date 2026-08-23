95474dff3ef42cd8087eb39fec0e5ae94ebf801a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:54 +0800

    Add ?????/226._?????/code_templates/solution.scala

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 翻转二叉树
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def invertTree(root: TreeNode): TreeNode = {
+        
+    }
+}
