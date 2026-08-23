b2d4c6e46ada1b30dc0e944d3246cd0532966144
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:20 +0800

    Add ???/219._??????_II/code_templates/solution.js

---
+++ 哈希表/219._存在重复元素_II/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 存在重复元素 II
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/contains-duplicate-ii/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    
+};
