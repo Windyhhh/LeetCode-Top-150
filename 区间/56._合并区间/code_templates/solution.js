9ddebac765989bfb315b72d8f4859ed76d39a3c8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:45 +0800

    Add ??/56._????/code_templates/solution.js

---
+++ 区间/56._合并区间/code_templates/solution.js
@@ -0,0 +1,12 @@
+# 合并区间
+# Language: javascript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    
+};
