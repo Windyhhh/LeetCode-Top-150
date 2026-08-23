b1abbb9bf76e7bfd58755973cf8db84c4f1df54b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:48 +0800

    Add ??/56._????/code_templates/solution.py

---
+++ 区间/56._合并区间/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 合并区间
+# Language: python3
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
