834f17675c45e04e19c1c4bf003625d6945084a5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:50 +0800

    Add ?????/226._?????/code_templates/solution.php

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.php
@@ -0,0 +1,28 @@
+# 翻转二叉树
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function invertTree($root) {
+        
+    }
+}
