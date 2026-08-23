6fe53e65ca9e2f487d4dadacdaf27738d62aef4d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:17 +0800

    Add ???/383._???/code_templates/solution.js

---
+++ 哈希表/383._赎金信/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 赎金信
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    
+};
