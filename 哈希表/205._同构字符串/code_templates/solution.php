d401fdc0ba93b59a84d7a26d930c7102980bdaea
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:04 +0800

    Add ???/205._?????/code_templates/solution.php

---
+++ 哈希表/205._同构字符串/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 同构字符串
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @param String $t
+     * @return Boolean
+     */
+    function isIsomorphic($s, $t) {
+        
+    }
+}
