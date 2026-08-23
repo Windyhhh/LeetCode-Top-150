12419821249c3dae3fc0a57284001eeb83103be7
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:30 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.rb

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 两数之和 II - 输入有序数组
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+# @param {Integer[]} numbers
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(numbers, target)
+    
+end
