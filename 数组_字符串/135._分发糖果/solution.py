4534083d000a10ad3c74459ffa742274ff32ad4f
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:29 +0800

    Add ??_???/135._????/solution.py

---
+++ 数组_字符串/135._分发糖果/solution.py
@@ -0,0 +1,21 @@
+# 分发糖果
+# 题目编号: 135
+# 难度: Hard
+# 题目链接: https://leetcode.cn/problems/candy/
+# 标签: 贪心, 数组
+
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        
+
+# 解题思路:
+# 1. 
+# 2. 
+# 3. 
+
+# 时间复杂度: O()
+# 空间复杂度: O()
