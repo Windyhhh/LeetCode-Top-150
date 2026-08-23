01f3703a479a3e215eec439d43260b4a681f2d43
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:36 +0800

    Add ?????/129._????????????/code_templates/solution.swift

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.swift
@@ -0,0 +1,25 @@
+# 求根节点到叶节点数字之和
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func sumNumbers(_ root: TreeNode?) -> Int {
+        
+    }
+}
