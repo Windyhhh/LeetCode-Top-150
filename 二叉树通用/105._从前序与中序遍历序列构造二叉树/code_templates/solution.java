e8a7233d81cd481f097cad1a72b1cbdbe1ad6495
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:54 +0800

    Add ?????/105._???????????????/code_templates/solution.java

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.java
@@ -0,0 +1,25 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+    }
+}
