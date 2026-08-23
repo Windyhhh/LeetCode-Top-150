166f7c554bfec4c66057c7979128579af75eba53
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:05 +0800

    Add ???/290._????/code_templates/solution.swift

---
+++ 哈希表/290._单词规律/code_templates/solution.swift
@@ -0,0 +1,10 @@
+# 单词规律
+# Language: swift
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+class Solution {
+    func wordPattern(_ pattern: String, _ s: String) -> Bool {
+        
+    }
+}
