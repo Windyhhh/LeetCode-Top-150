98db23a76627dccd912aabf1eb895b8eb0a9ee9a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:08 +0800

    Add ??/228._????/code_templates/solution.kt

---
+++ 区间/228._汇总区间/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 汇总区间
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+class Solution {
+    fun summaryRanges(nums: IntArray): List<String> {
+        
+    }
+}
