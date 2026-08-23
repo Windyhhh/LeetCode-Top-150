2539fdb852bf562f46e24a145a5997cb3ce2e8dc
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:38 +0800

    Add ???/392._?????/solution.py

---
+++ 双指针/392._判断子序列/solution.py
@@ -0,0 +1,22 @@
+# 判断子序列
+# 题目编号: 392
+# 难度: Easy
+# 题目链接: https://leetcode.cn/problems/is-subsequence/
+# 标签: 双指针, 字符串, 动态规划
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+
+# 解题思路:
+# 1. 
+# 2. 
+# 3. 
+
+# 时间复杂度: O()
+# 空间复杂度: O()
