89d9d595563f982dbe4eb77feced1efa564c7c1b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:11 +0800

    Add ??/228._????/code_templates/solution.rb

---
+++ 区间/228._汇总区间/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 汇总区间
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+# @param {Integer[]} nums
+# @return {String[]}
+def summary_ranges(nums)
+    
+end
