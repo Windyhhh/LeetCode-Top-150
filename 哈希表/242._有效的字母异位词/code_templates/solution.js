599f18e43222f11403f71da0d3a13ee346cd9f56
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:40 +0800

    Add ???/242._????????/code_templates/solution.js

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 有效的字母异位词
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+};
