47743cb947a0332f7bb3c464fc5639dfa34afd07
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:16 +0800

    Add ??_???/121._?????????/code_templates/solution.kt

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 买卖股票的最佳时机
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        
+    }
+}
