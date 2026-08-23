9f5fab1f14f4da4709e7aa72a1d30eae1159c018
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:16 +0800

    Add ?????/106._???????????????/code_templates/solution.php

---
+++ 二叉树通用/106._从中序与后序遍历序列构造二叉树/code_templates/solution.php
@@ -0,0 +1,29 @@
+# 从中序与后序遍历序列构造二叉树
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $inorder
+     * @param Integer[] $postorder
+     * @return TreeNode
+     */
+    function buildTree($inorder, $postorder) {
+        
+    }
+}
