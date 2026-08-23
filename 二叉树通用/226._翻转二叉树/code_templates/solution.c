ae7b0f493bbcf1ec1a95f90a0bd5abf570d17158
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:43 +0800

    Add ?????/226._?????/code_templates/solution.c

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.c
@@ -0,0 +1,16 @@
+# 翻转二叉树
+# Language: c
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    
+}
