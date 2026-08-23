0df542cdab7bdabafc9aa3c466e6a3c7244dc812
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:34 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.ts

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 两数之和 II - 输入有序数组
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+function twoSum(numbers: number[], target: number): number[] {
+    
+};
