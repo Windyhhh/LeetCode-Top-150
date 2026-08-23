1b45bd7561c2976c1ed918eefa4cd3689052cd27
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:02 +0800

    Add ??_???/14._??????/code_templates/solution.rs

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 最长公共前缀
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        
+    }
+}
