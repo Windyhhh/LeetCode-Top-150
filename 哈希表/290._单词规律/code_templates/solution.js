fbf21999e47afbdef46344e925672e2b4b1e306e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:58 +0800

    Add ???/290._????/code_templates/solution.js

---
+++ 哈希表/290._单词规律/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 单词规律
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    
+};
