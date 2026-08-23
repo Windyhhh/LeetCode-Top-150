7998b62850564490fae474dcdce66c0f23ac0862
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:59 +0800

    Add ??_???/189._????/code_templates/solution.rb

---
+++ 数组_字符串/189._轮转数组/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 轮转数组
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/rotate-array/
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+    
+end
