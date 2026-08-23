566e51ee0a82c927c73943caa115ef8b83370d13
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:02:47 +0800

    Add ?????/100._????/code_templates/solution.c

---
+++ 二叉树通用/100._相同的树/code_templates/solution.c
@@ -0,0 +1,16 @@
+# 相同的树
+# Language: c
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    
+}
