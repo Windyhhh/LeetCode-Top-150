f93db8fd1175be10442c362620855db44ff7928e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:51 +0800

    Add ???/392._?????/code_templates/solution.rs

---
+++ 双指针/392._判断子序列/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 判断子序列
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+impl Solution {
+    pub fn is_subsequence(s: String, t: String) -> bool {
+        
+    }
+}
