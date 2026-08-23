507eb638f752e3579b84dc88c0adc80126a695fb
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:29 +0800

    Add ???/128._??????/code_templates/solution.rb

---
+++ 哈希表/128._最长连续序列/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 最长连续序列
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+# @param {Integer[]} nums
+# @return {Integer}
+def longest_consecutive(nums)
+    
+end
