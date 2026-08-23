6be13a85e0c717bbe714631dc725df93379ac5dc
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:59 +0800

    Add ???/290._????/code_templates/solution.kt

---
+++ 哈希表/290._单词规律/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 单词规律
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+class Solution {
+    fun wordPattern(pattern: String, s: String): Boolean {
+        
+    }
+}
