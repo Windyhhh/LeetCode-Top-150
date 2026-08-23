88f0d3335f46dd462a1bd9dc9cab7daac143490e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:35 +0800

    Add ?????/112._????/code_templates/solution.py

---
+++ 二叉树通用/112._路径总和/code_templates/solution.py
@@ -0,0 +1,14 @@
+# 路径总和
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/path-sum/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
