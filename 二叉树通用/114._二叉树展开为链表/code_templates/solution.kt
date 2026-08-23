1b42808eebfce7b2b2eb6867507258a4849b83e2
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:53 +0800

    Add ?????/114._????????/code_templates/solution.kt

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.kt
@@ -0,0 +1,20 @@
+# 二叉树展开为链表
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun flatten(root: TreeNode?): Unit {
+        
+    }
+}
