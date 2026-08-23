29624eff208c08492482d3f54fd981f5f5f48deb
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:19 +0800

    Add ?????/117._????????????????_II/code_templates/solution.ts

---
+++ 二叉树通用/117._填充每个节点的下一个右侧节点指针_II/code_templates/solution.ts
@@ -0,0 +1,26 @@
+# 填充每个节点的下一个右侧节点指针 II
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     left: _Node | null
+ *     right: _Node | null
+ *     next: _Node | null
+ * 
+ *     constructor(val?: number, left?: _Node, right?: _Node, next?: _Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+
+function connect(root: _Node | null): _Node | null {
+    
+};
