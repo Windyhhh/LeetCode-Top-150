9bb8781a71d95e22b527d8dd4f5a26d2b60e58b6
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:23 +0800

    Add ???/219._??????_II/code_templates/solution.php

---
+++ 哈希表/219._存在重复元素_II/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 存在重复元素 II
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/contains-duplicate-ii/
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Boolean
+     */
+    function containsNearbyDuplicate($nums, $k) {
+        
+    }
+}
