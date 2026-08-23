fa8cc0af467a8f5f7c332f3713636e9015ed3338
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:51 +0800

    Add ?????/226._?????/code_templates/solution.py

---
+++ 二叉树通用/226._翻转二叉树/code_templates/solution.py
@@ -0,0 +1,14 @@
+# 翻转二叉树
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/invert-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
