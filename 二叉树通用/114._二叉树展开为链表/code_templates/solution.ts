33d6c9bca2c63e7d4bed74591ddf735cbe9d8abd
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:00 +0800

    Add ?????/114._????????/code_templates/solution.ts

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.ts
@@ -0,0 +1,25 @@
+# 二叉树展开为链表
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify root in-place instead.
+ */
+function flatten(root: TreeNode | null): void {
+    
+};
