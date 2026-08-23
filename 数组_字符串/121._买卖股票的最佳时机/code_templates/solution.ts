afc275c47d7e8746797c5d9c020a5e9779b0dd2e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:23 +0800

    Add ??_???/121._?????????/code_templates/solution.ts

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 买卖股票的最佳时机
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+function maxProfit(prices: number[]): number {
+    
+};
