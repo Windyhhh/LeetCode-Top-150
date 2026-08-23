d1f05bbe5985dd4c8cccedf77950522b0eddcdb0
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:03 +0800

    Add ??/57._????/code_templates/solution.cs

---
+++ 区间/57._插入区间/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 插入区间
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval) {
+        
+    }
+}
