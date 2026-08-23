994cb4f54ea05479d29fb4c6ee5f296e7a1301dc
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:03:23 +0800

    Add ?????/101._?????/code_templates/solution.scala

---
+++ 二叉树通用/101._对称二叉树/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 对称二叉树
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/symmetric-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def isSymmetric(root: TreeNode): Boolean = {
+        
+    }
+}
