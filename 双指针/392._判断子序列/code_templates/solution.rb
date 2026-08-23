628081360e0aeb31f7b05b71212d267282064984
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:50 +0800

    Add ???/392._?????/code_templates/solution.rb

---
+++ 双指针/392._判断子序列/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 判断子序列
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_subsequence(s, t)
+    
+end
