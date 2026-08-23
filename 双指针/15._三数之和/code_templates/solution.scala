440aeeae9df42a3bd338ec4f3321a8f9c5ee7b90
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:12 +0800

    Add ???/15._????/code_templates/solution.scala

---
+++ 双指针/15._三数之和/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 三数之和
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/3sum/
+
+object Solution {
+    def threeSum(nums: Array[Int]): List[List[Int]] = {
+        
+    }
+}
