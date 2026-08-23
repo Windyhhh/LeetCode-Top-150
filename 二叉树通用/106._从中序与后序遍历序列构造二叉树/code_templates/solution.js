1c5ebe6e25efdd26f3b779ab8b32a767c81fda6a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:14 +0800

    Add ?????/106._???????????????/code_templates/solution.js

---
+++ 二叉树通用/106._从中序与后序遍历序列构造二叉树/code_templates/solution.js
@@ -0,0 +1,21 @@
+# 从中序与后序遍历序列构造二叉树
+# Language: javascript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+    
+};
