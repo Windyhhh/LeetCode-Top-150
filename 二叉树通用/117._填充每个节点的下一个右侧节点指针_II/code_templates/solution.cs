d2aefbda17c3622eb282573062b4db4dd6ec1c79
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:09 +0800

    Add ?????/117._????????????????_II/code_templates/solution.cs

---
+++ 二叉树通用/117._填充每个节点的下一个右侧节点指针_II/code_templates/solution.cs
@@ -0,0 +1,33 @@
+# 填充每个节点的下一个右侧节点指针 II
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+
+public class Solution {
+    public Node Connect(Node root) {
+        
+    }
+}
