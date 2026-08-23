f4d8786101e631cfca8cc997b806c18ce71ab16d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:40 +0800

    Add ??/56._????/code_templates/solution.c

---
+++ 区间/56._合并区间/code_templates/solution.c
@@ -0,0 +1,13 @@
+# 合并区间
+# Language: c
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* returnSize, int** returnColumnSizes) {
+    
+}
