695d1ea8a9caad9e0fb9bc7b08f94a7dcfbc1f43
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:32 +0800

    Add ?????/129._????????????/code_templates/solution.py

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.py
@@ -0,0 +1,14 @@
+# 求根节点到叶节点数字之和
+# Language: python3
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
