14a1521cc6ff7556d8496d6a032896da50a08382
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:02:59 +0800

    Add ?????/100._????/code_templates/solution.scala

---
+++ 二叉树通用/100._相同的树/code_templates/solution.scala
@@ -0,0 +1,18 @@
+# 相同的树
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def isSameTree(p: TreeNode, q: TreeNode): Boolean = {
+        
+    }
+}
