02a9b75c319a3e1f86b46bf2d4a2b4dd8a2fc117
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:03:26 +0800

    Add ?????/101._?????/code_templates/solution.ts

---
+++ 二叉树通用/101._对称二叉树/code_templates/solution.ts
@@ -0,0 +1,22 @@
+# 对称二叉树
+# Language: typescript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/symmetric-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    
+};
