34f4ae9e49b7ef20bc7dbad61a51cb32bdf920de
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:25 +0800

    Add ???/219._??????_II/code_templates/solution.rb

---
+++ 哈希表/219._存在重复元素_II/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 存在重复元素 II
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/contains-duplicate-ii/
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Boolean}
+def contains_nearby_duplicate(nums, k)
+    
+end
