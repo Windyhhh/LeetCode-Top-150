4ab45c73e697d8e46fcba519d37e3d485cebb3ec
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:43 +0800

    Add ???/202._???/code_templates/solution.js

---
+++ 哈希表/202._快乐数/code_templates/solution.js
@@ -0,0 +1,12 @@
+# 快乐数
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/happy-number/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    
+};
