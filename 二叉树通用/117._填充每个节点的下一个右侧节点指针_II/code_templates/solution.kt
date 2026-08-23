20a03ce56c5fbb66ee63750eddc955d0b11949fd
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:12 +0800

    Add ?????/117._????????????????_II/code_templates/solution.kt

---
+++ 二叉树通用/117._填充每个节点的下一个右侧节点指针_II/code_templates/solution.kt
@@ -0,0 +1,19 @@
+# 填充每个节点的下一个右侧节点指针 II
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+/**
+ * Definition for a Node.
+ * class Node(var `val`: Int) {
+ *     var left: Node? = null
+ *     var right: Node? = null
+ *     var next: Node? = null
+ * }
+ */
+
+class Solution {
+    fun connect(root: Node?): Node? {
+        
+    }
+}
