68c9d1c7cc1904356950695774d9ff8214908cef
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:22 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.cpp

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 两数之和 II - 输入有序数组
+# Language: cpp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+    }
+};
