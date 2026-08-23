7ab2174f190e3c9eb97d4a86a4974a4d5032407a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:27 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.php

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 两数之和 II - 输入有序数组
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+
+    /**
+     * @param Integer[] $numbers
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($numbers, $target) {
+        
+    }
+}
