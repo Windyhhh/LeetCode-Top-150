78c2c85a4408995c57f4b2c6b5b8071ce70250a5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:56 +0800

    Add ??_???/14._??????/code_templates/solution.js

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.js
@@ -0,0 +1,12 @@
+# 最长公共前缀
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+};
