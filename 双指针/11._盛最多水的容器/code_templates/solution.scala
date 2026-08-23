5b2b1e1e9efb674ce75fc071dbe08f0d9da08f90
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:33 +0800

    Add ???/11._???????/code_templates/solution.scala

---
+++ 双指针/11._盛最多水的容器/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 盛最多水的容器
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/container-with-most-water/
+
+object Solution {
+    def maxArea(height: Array[Int]): Int = {
+        
+    }
+}
