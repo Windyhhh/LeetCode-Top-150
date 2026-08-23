12d07eb71ed207b82a41ab02cb04d7a0829c3187
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:24 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.java

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 两数之和 II - 输入有序数组
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+    }
+}
