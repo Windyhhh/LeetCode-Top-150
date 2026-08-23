4c7b8c6b59fba676a7c878fd5540ed5321795d46
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:56 +0800

    Add ?????/105._???????????????/code_templates/solution.kt

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.kt
@@ -0,0 +1,20 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
+        
+    }
+}
