ed04180e40d5c1c23618f448759ef05621b870ab
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:25 +0800

    Add ???/128._??????/code_templates/solution.js

---
+++ 哈希表/128._最长连续序列/code_templates/solution.js
@@ -0,0 +1,12 @@
+# 最长连续序列
+# Language: javascript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/longest-consecutive-sequence/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    
+};
