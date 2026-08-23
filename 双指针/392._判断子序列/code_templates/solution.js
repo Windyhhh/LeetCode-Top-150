9d33ebdaa8967bfc0fa5c45a4edaacd16454cf77
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:46 +0800

    Add ???/392._?????/code_templates/solution.js

---
+++ 双指针/392._判断子序列/code_templates/solution.js
@@ -0,0 +1,13 @@
+# 判断子序列
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    
+};
