d265143f503775523067ad9161d26030db6ad0c2
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:55 +0800

    Add ???/290._????/code_templates/solution.cpp

---
+++ 哈希表/290._单词规律/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 单词规律
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        
+    }
+};
