2038afd7cd9b4110a3fa04243c30eb0fd4ca0bfc
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:19:01 +0800

    Add ??_???/189._????/code_templates/solution.scala

---
+++ 数组_字符串/189._轮转数组/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 轮转数组
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/rotate-array/
+
+object Solution {
+    def rotate(nums: Array[Int], k: Int): Unit = {
+        
+    }
+}
