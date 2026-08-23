d75378d0202e6cad8fb8e057d9689c874daf24e2
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:53 +0800

    Add ??_???/14._??????/code_templates/solution.cpp

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 最长公共前缀
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};
