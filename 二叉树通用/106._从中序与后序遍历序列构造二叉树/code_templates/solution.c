3e258b861f0399bb5ee9983b492c637ada77527f
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:10 +0800

    Add ?????/106._???????????????/code_templates/solution.c

---
+++ 二叉树通用/106._从中序与后序遍历序列构造二叉树/code_templates/solution.c
@@ -0,0 +1,16 @@
+# 从中序与后序遍历序列构造二叉树
+# Language: c
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* buildTree(int* inorder, int inorderSize, int* postorder, int postorderSize) {
+    
+}
