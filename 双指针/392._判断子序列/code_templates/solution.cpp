9b09082f4ef5152d6c1622b266f3c8a3f8493f3c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:42 +0800

    Add ???/392._?????/code_templates/solution.cpp

---
+++ 双指针/392._判断子序列/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 判断子序列
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+    }
+};
