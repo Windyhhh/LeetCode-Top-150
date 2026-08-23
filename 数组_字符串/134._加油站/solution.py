397d1c25a5020a19fab85507436df0b7bc065c2d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:08 +0800

    Add ??_???/134._???/solution.py

---
+++ 数组_字符串/134._加油站/solution.py
@@ -0,0 +1,22 @@
+# 加油站
+# 题目编号: 134
+# 难度: Medium
+# 题目链接: https://leetcode.cn/problems/gas-station/
+# 标签: 贪心, 数组
+
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        
+
+# 解题思路:
+# 1. 
+# 2. 
+# 3. 
+
+# 时间复杂度: O()
+# 空间复杂度: O()
