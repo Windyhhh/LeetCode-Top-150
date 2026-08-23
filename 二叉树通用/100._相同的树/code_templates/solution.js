d32871fe064a659626773ec9112aefc7a82ae992
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:02:52 +0800

    Add ?????/100._????/code_templates/solution.js

---
+++ 二叉树通用/100._相同的树/code_templates/solution.js
@@ -0,0 +1,21 @@
+# 相同的树
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    
+};
