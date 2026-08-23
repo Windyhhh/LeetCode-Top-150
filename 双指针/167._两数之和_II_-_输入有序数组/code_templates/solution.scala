1ea799a863fe481cd3e6ee786cd07788053ad2ec
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:32 +0800

    Add ???/167._????_II_-_??????/code_templates/solution.scala

---
+++ 双指针/167._两数之和_II_-_输入有序数组/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 两数之和 II - 输入有序数组
+# Language: scala
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+
+object Solution {
+    def twoSum(numbers: Array[Int], target: Int): Array[Int] = {
+        
+    }
+}
