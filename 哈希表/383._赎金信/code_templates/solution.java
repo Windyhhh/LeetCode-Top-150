cac5a41491e14165c4a4a316e5fa27cc63d9c5be
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:16 +0800

    Add ???/383._???/code_templates/solution.java

---
+++ 哈希表/383._赎金信/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 赎金信
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+    }
+}
