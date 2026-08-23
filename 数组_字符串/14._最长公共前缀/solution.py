1e7c0de76454a10beebac0369b899b44247911ab
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:49 +0800

    Add ??_???/14._??????/solution.py

---
+++ 数组_字符串/14._最长公共前缀/solution.py
@@ -0,0 +1,21 @@
+# 最长公共前缀
+# 题目编号: 14
+# 难度: Easy
+# 题目链接: https://leetcode.cn/problems/longest-common-prefix/
+# 标签: 字典树, 数组, 字符串
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+
+# 解题思路:
+# 1. 
+# 2. 
+# 3. 
+
+# 时间复杂度: O()
+# 空间复杂度: O()
