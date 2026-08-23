d21b054b9d782d9a381da214964384c95c1201d8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:36 +0800

    Add ??_???/169._????/code_templates/solution.kt

---
+++ 数组_字符串/169._多数元素/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 多数元素
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/majority-element/
+
+class Solution {
+    fun majorityElement(nums: IntArray): Int {
+        
+    }
+}
