34a4f22ee5cfca7dedd5d2c4fdab0345f329bf26
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:41 +0800

    Add ???/202._???/code_templates/solution.cs

---
+++ 哈希表/202._快乐数/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 快乐数
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/happy-number/
+
+public class Solution {
+    public bool IsHappy(int n) {
+        
+    }
+}
