6f226da244c2ce270efcac6bc880106822050ef3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:55 +0800

    Add ??_???/14._??????/code_templates/solution.java

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 最长公共前缀
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+    }
+}
