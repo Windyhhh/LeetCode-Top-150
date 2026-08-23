64b049c01ab45533dfd2e9949cb0f4a2c6fa66fc
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:38 +0800

    Add ???/49._???????/code_templates/solution.php

---
+++ 哈希表/49._字母异位词分组/code_templates/solution.php
@@ -0,0 +1,15 @@
+# 字母异位词分组
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/group-anagrams/
+
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String[][]
+     */
+    function groupAnagrams($strs) {
+        
+    }
+}
