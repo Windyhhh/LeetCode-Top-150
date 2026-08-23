defde5771908a6969f012ef4e2e6c8078329da80
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:49 +0800

    Add ??/56._????/code_templates/solution.rb

---
+++ 区间/56._合并区间/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 合并区间
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+# @param {Integer[][]} intervals
+# @return {Integer[][]}
+def merge(intervals)
+    
+end
