2a15eabbcd257d5b0821182e0151eda5a9afa370
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:00 +0800

    Add ???/290._????/code_templates/solution.php

---
+++ 哈希表/290._单词规律/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 单词规律
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/word-pattern/
+
+class Solution {
+
+    /**
+     * @param String $pattern
+     * @param String $s
+     * @return Boolean
+     */
+    function wordPattern($pattern, $s) {
+        
+    }
+}
