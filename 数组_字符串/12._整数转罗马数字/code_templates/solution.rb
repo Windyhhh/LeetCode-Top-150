c37e6f2a5b20217970b877895acf35b6e3a8d834
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:59 +0800

    Add ??_???/12._???????/code_templates/solution.rb

---
+++ 数组_字符串/12._整数转罗马数字/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 整数转罗马数字
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/integer-to-roman/
+
+# @param {Integer} num
+# @return {String}
+def int_to_roman(num)
+    
+end
