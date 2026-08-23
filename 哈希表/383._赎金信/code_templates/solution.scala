e1ff7b8c75885f7ef1fb957190eee10e6475600f
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:23 +0800

    Add ???/383._???/code_templates/solution.scala

---
+++ 哈希表/383._赎金信/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 赎金信
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+object Solution {
+    def canConstruct(ransomNote: String, magazine: String): Boolean = {
+        
+    }
+}
