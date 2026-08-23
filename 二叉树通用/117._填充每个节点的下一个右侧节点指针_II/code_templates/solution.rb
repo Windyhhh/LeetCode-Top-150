08afc16fd8ba2315977c97e9f2693d5b369640e4
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:16 +0800

    Add ?????/117._????????????????_II/code_templates/solution.rb

---
+++ 二叉树通用/117._填充每个节点的下一个右侧节点指针_II/code_templates/solution.rb
@@ -0,0 +1,19 @@
+# 填充每个节点的下一个右侧节点指针 II
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+# Definition for a Node.
+# class Node
+#     attr_accessor :val, :left, :right, :next
+#     def initialize(val)
+#         @val = val
+#         @left, @right, @next = nil, nil, nil
+#     end
+# end
+
+# @param {Node} root
+# @return {Node}
+def connect(root)
+    
+end
