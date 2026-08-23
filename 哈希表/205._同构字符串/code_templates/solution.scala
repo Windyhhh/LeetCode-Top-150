4fe901d5a2f2b4e3999a4b0aae2a384dacbf0934
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:08 +0800

    Add ???/205._?????/code_templates/solution.scala

---
+++ 哈希表/205._同构字符串/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 同构字符串
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+object Solution {
+    def isIsomorphic(s: String, t: String): Boolean = {
+        
+    }
+}
