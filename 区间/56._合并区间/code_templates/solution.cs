46156a22e2a53e459c4fafdb4dbe710132895b11
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:43 +0800

    Add ??/56._????/code_templates/solution.cs

---
+++ 区间/56._合并区间/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 合并区间
+# Language: csharp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        
+    }
+}
