5f9e63a1d859331f97ef8524739b810159d8f711
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:58 +0800

    Add ??_???/14._??????/code_templates/solution.kt

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 最长公共前缀
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        
+    }
+}
