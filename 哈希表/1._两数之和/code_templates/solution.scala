fe8a9be17358923471a9df7ef8d9ea746771ab8b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:13:12 +0800

    Add ???/1._????/code_templates/solution.scala

---
+++ 哈希表/1._两数之和/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 两数之和
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/two-sum/
+
+object Solution {
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+        
+    }
+}
