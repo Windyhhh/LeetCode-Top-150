9c5bf7f2873b7ea643c99ead03fe5b1fb17cc4f6
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:19 +0800

    Add ???/383._???/code_templates/solution.php

---
+++ 哈希表/383._赎金信/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 赎金信
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+class Solution {
+
+    /**
+     * @param String $ransomNote
+     * @param String $magazine
+     * @return Boolean
+     */
+    function canConstruct($ransomNote, $magazine) {
+        
+    }
+}
