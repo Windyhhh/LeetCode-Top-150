609e41f9752afc390e5320d187e742c0d9c6820b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:10 +0800

    Add ??/228._????/code_templates/solution.py

---
+++ 区间/228._汇总区间/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 汇总区间
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        
