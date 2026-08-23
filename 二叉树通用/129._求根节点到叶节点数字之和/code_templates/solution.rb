61281f295a909067f86a4210b8df0597ddc432a4
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:09:33 +0800

    Add ?????/129._????????????/code_templates/solution.rb

---
+++ 二叉树通用/129._求根节点到叶节点数字之和/code_templates/solution.rb
@@ -0,0 +1,19 @@
+# 求根节点到叶节点数字之和
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def sum_numbers(root)
+    
+end
