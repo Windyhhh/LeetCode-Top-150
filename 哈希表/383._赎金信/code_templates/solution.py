db45780a62963cc0ecee52e0149a1a4f383135ea
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:20 +0800

    Add ???/383._???/code_templates/solution.py

---
+++ 哈希表/383._赎金信/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 赎金信
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
