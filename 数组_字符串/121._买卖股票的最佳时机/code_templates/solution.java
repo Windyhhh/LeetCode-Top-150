46d82ae03a3cc5dd0f9b21301b7297fb4ab48d1b
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:14 +0800

    Add ??_???/121._?????????/code_templates/solution.java

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 买卖股票的最佳时机
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+    }
+}
