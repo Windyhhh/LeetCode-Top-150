3bb88969dafa97811bc532d6948bca6a26f40c22
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:01 +0800

    Add ???/205._?????/code_templates/solution.java

---
+++ 哈希表/205._同构字符串/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 同构字符串
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/isomorphic-strings/
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+    }
+}
