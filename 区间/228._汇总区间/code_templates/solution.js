43d8e890bdb088bea6a8e0ad502fefe78a294784
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:07 +0800

    Add ??/228._????/code_templates/solution.js

---
+++ 区间/228._汇总区间/code_templates/solution.js
@@ -0,0 +1,12 @@
+# 汇总区间
+# Language: javascript
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    
+};
