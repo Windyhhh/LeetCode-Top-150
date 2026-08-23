07f0712ce173262719ebd04480e0ee98b4798101
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:49 +0800

    Add ?????/114._????????/code_templates/solution.cpp

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.cpp
@@ -0,0 +1,22 @@
+# 二叉树展开为链表
+# Language: cpp
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        
+    }
+};
