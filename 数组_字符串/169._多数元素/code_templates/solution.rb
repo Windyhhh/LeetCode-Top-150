daf2fb13c84be8f73d37e3f6fc5f491ba129f2ad
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:39 +0800

    Add ??_???/169._????/code_templates/solution.rb

---
+++ 数组_字符串/169._多数元素/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 多数元素
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/majority-element/
+
+# @param {Integer[]} nums
+# @return {Integer}
+def majority_element(nums)
+    
+end
