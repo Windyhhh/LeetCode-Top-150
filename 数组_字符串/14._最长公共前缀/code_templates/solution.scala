83ef5866963233670d7f5c789bf56203ad0c8793
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:03 +0800

    Add ??_???/14._??????/code_templates/solution.scala

---
+++ 数组_字符串/14._最长公共前缀/code_templates/solution.scala
@@ -0,0 +1,10 @@
+# 最长公共前缀
+# Language: scala
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/longest-common-prefix/
+
+object Solution {
+    def longestCommonPrefix(strs: Array[String]): String = {
+        
+    }
+}
