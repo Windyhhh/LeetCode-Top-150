4293d46fdf6c799ba84fe8cddc96ea8fb239d7e8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:24 +0800

    Add ??_???/134._???/code_templates/solution.ts

---
+++ 数组_字符串/134._加油站/code_templates/solution.ts
@@ -0,0 +1,8 @@
+# 加油站
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/gas-station/
+
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+    
+};
