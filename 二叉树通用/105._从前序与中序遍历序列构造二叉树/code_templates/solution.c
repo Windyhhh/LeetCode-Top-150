dc26862d18a06db1942c70408945bb05ab872c64
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:51 +0800

    Add ?????/105._???????????????/code_templates/solution.c

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.c
@@ -0,0 +1,16 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: c
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize) {
+    
+}
