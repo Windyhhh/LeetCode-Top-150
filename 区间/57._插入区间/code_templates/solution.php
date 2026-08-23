f0735fbb5dccd575e65fb338e5da217b2863c13c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:11:07 +0800

    Add ??/57._????/code_templates/solution.php

---
+++ 区间/57._插入区间/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 插入区间
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/insert-interval/
+
+class Solution {
+
+    /**
+     * @param Integer[][] $intervals
+     * @param Integer[] $newInterval
+     * @return Integer[][]
+     */
+    function insert($intervals, $newInterval) {
+        
+    }
+}
