97122b98f5065e6faa428587e1c035c643106d83
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:02 +0800

    Add ?????/105._???????????????/code_templates/solution.swift

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.swift
@@ -0,0 +1,25 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: swift
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+        
+    }
+}
