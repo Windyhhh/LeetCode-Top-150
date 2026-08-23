09f2a2a7e58bd540c58a33a94409a195923bedc8
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:44 +0800

    Add ??/56._????/code_templates/solution.java

---
+++ 区间/56._合并区间/code_templates/solution.java
@@ -0,0 +1,10 @@
+# 合并区间
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/merge-intervals/
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        
+    }
+}
