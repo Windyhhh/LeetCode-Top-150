e386ed98a5bb7d5b9316e3d0f3ed87ac710b0da2
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:12 +0800

    Add ??/228._????/code_templates/solution.rs

---
+++ 区间/228._汇总区间/code_templates/solution.rs
@@ -0,0 +1,10 @@
+# 汇总区间
+# Language: rust
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+impl Solution {
+    pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
+        
+    }
+}
