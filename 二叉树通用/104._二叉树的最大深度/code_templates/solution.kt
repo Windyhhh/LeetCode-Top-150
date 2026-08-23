390c00fb28dc4e1c14655c59b5c34d429c430419
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:07:37 +0800

    Add ?????/104._????????/code_templates/solution.kt

---
+++ 二叉树通用/104._二叉树的最大深度/code_templates/solution.kt
@@ -0,0 +1,20 @@
+# 二叉树的最大深度
+# Language: kotlin
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        
+    }
+}
