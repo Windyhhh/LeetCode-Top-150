7d4643bf8d6bbba3e42816123f04ebc38c2b08ce
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:12 +0800

    Add ??_???/121._?????????/code_templates/solution.cpp

---
+++ 数组_字符串/121._买卖股票的最佳时机/code_templates/solution.cpp
@@ -0,0 +1,11 @@
+# 买卖股票的最佳时机
+# Language: cpp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+    }
+};
