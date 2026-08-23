c1c4515bcf37f2edaba838cf1135f2936322e030
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:02 +0800

    Add ??/228._????/code_templates/solution.c

---
+++ 区间/228._汇总区间/code_templates/solution.c
@@ -0,0 +1,11 @@
+# 汇总区间
+# Language: c
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** summaryRanges(int* nums, int numsSize, int* returnSize) {
+    
+}
