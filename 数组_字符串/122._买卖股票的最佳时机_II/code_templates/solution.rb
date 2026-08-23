5b15ccdee2ee03b0db70bed041f801178c2c28f1
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:16:40 +0800

    Add ??_???/122._?????????_II/code_templates/solution.rb

---
+++ 数组_字符串/122._买卖股票的最佳时机_II/code_templates/solution.rb
@@ -0,0 +1,10 @@
+# 买卖股票的最佳时机 II
+# Language: ruby
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
+
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    
+end
