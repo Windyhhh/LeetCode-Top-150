8681ab17e0a9c9a726c46b24b8341f9824dc02de
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:42 +0800

    Add ???/242._????????/code_templates/solution.php

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 有效的字母异位词
+# Language: php
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @param String $t
+     * @return Boolean
+     */
+    function isAnagram($s, $t) {
+        
+    }
+}
