8fa55d187c3f4b6f054c33271f1b977e9c4a4f6c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:14:44 +0800

    Add ???/242._????????/code_templates/solution.rb

---
+++ 哈希表/242._有效的字母异位词/code_templates/solution.rb
@@ -0,0 +1,11 @@
+# 有效的字母异位词
+# Language: ruby
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/valid-anagram/
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_anagram(s, t)
+    
+end
