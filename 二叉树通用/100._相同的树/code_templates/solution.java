f9e1ee7aab373068e94089b4800427241d696799
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:02:50 +0800

    Add ?????/100._????/code_templates/solution.java

---
+++ 二叉树通用/100._相同的树/code_templates/solution.java
@@ -0,0 +1,25 @@
+# 相同的树
+# Language: java
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+    }
+}
