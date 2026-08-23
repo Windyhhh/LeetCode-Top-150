d53d1044cf0fba4e0c52cef792c3cb943a5bdc2a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:10:04 +0800

    Add ??/228._????/code_templates/solution.cs

---
+++ 区间/228._汇总区间/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 汇总区间
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/summary-ranges/
+
+public class Solution {
+    public IList<string> SummaryRanges(int[] nums) {
+        
+    }
+}
