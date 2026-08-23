0aad04d043e802ff881a9d44cfeca4ff8da20cb3
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:51 +0800

    Add ?????/114._????????/code_templates/solution.java

---
+++ 二叉树通用/114._二叉树展开为链表/code_templates/solution.java
@@ -0,0 +1,25 @@
+# 二叉树展开为链表
+# Language: java
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        
+    }
+}
