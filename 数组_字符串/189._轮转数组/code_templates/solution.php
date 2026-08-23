368ccfec9b87be11f8b463c1553f8fabb3ed3ce1
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:18:57 +0800

    Add ??_???/189._????/code_templates/solution.php

---
+++ 数组_字符串/189._轮转数组/code_templates/solution.php
@@ -0,0 +1,16 @@
+# 轮转数组
+# Language: php
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/rotate-array/
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return NULL
+     */
+    function rotate(&$nums, $k) {
+        
+    }
+}
