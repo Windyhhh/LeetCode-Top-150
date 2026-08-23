0381c900bf31db2b440c4812fdf07ee380f62d7e
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:12:49 +0800

    Add ???/392._?????/code_templates/solution.py

---
+++ 双指针/392._判断子序列/code_templates/solution.py
@@ -0,0 +1,8 @@
+# 判断子序列
+# Language: python3
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
