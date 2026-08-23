bc10bd0732a66c1cba20774ed3ecdcefb71821af
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:15:14 +0800

    Add ???/383._???/code_templates/solution.cs

---
+++ 哈希表/383._赎金信/code_templates/solution.cs
@@ -0,0 +1,10 @@
+# 赎金信
+# Language: csharp
+# Difficulty: Easy
+# Problem Link: https://leetcode.cn/problems/ransom-note/
+
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        
+    }
+}
