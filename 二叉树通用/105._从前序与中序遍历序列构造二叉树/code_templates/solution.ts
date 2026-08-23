eb0ea5c24354a963886530e55a34900132a8b9f9
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:08:03 +0800

    Add ?????/105._???????????????/code_templates/solution.ts

---
+++ 二叉树通用/105._从前序与中序遍历序列构造二叉树/code_templates/solution.ts
@@ -0,0 +1,22 @@
+# 从前序与中序遍历序列构造二叉树
+# Language: typescript
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+    
+};
