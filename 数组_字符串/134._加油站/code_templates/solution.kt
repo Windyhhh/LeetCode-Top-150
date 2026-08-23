3b63ba0933323dd1141446d614f330abdb54d24c
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 18:17:16 +0800

    Add ??_???/134._???/code_templates/solution.kt

---
+++ 数组_字符串/134._加油站/code_templates/solution.kt
@@ -0,0 +1,10 @@
+# 加油站
+# Language: kotlin
+# Difficulty: Medium
+# Problem Link: https://leetcode.cn/problems/gas-station/
+
+class Solution {
+    fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+        
+    }
+}
